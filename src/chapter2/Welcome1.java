a56596539a6f9662b0b954ef42cba2c3f64a32e9
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-06-17 10:38:46 +0100

    i made new changes

--- src/chapter2/Welcome1.java
+++ src/chapter2/Welcome1.java
@@ -3,6 +3,6 @@ package chapter2;
 public class Welcome1 {
     public static void main(String[] args)
     {
-        System.out.println("I must learn this Java programming Language! It's a must");
+        System.out.println("Hello World");
     }
 }
