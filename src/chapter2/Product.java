7be2ad8fe172e07b77149d60815ce8f31a294ece
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-06-18 14:55:10 +0100

    Product

---
+++ src/chapter2/Product.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Product {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int x = input.nextInt();
+
+        System.out.println("Enter the second integer: ");
+        int y = input.nextInt();
+
+        System.out.print("Enter the third integer: ");
+        int z = input.nextInt();
+
+        int result = x * y * z;
+        System.out.printf("Product is %d%n", result);
+    }
+}
