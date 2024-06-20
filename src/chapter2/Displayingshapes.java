7a41e844eddf63c1afb4413d5ff74c0384f4effa
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-06-20 17:06:01 +0100

    Displayingshapes

--- src/chapter2/Displayingshapes.java
+++ src/chapter2/Displayingshapes.java
@@ -4,6 +4,45 @@ import java.util.Scanner;
 
 public class Displayingshapes {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("* * * * * * * * *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("*               *%n");
+        System.out.printf("* * * * * * * * *%n");
 
+        System.out.printf("   ***    %n");
+        System.out.printf(" *      * %n");
+        System.out.printf("*        *%n");
+        System.out.printf("*        *%n");
+        System.out.printf("*        *%n");
+        System.out.printf("*        *%n");
+        System.out.printf("*        *%n");
+        System.out.printf(" *      * %n");
+        System.out.printf("   ***    %n");
+
+        System.out.printf("  *  %n");
+        System.out.printf(" *** %n");
+        System.out.printf("*****%n");
+        System.out.printf("  *  %n");
+        System.out.printf("  *  %n");
+        System.out.printf("  *  %n");
+        System.out.printf("  *  %n");
+        System.out.printf("  *  %n");
+        System.out.printf("  *  %n");
+
+        System.out.printf("    *    %n");
+        System.out.printf("   * *   %n");
+        System.out.printf("  *   *  %n");
+        System.out.printf(" *     * %n");
+        System.out.printf("*       *%n");
+        System.out.printf(" *     * %n");
+        System.out.printf("  *   *  %n");
+        System.out.printf("   * *   %n");
+        System.out.printf("    *    %n");
     }
 }
