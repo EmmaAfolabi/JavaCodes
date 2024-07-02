0cb6776eead0234443b499d11f7ab4c1b7e64772
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-02 13:18:07 +0100

    Multiples

--- src/chapter2/Multiples.java
+++ src/chapter2/Multiples.java
@@ -6,21 +6,21 @@ public class Multiples {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("kindly enter the first number: ");
-        int num1 = input.nextInt();
 
-        System.out.println("kindly enter the second number: ");
+
+        System.out.println("kindly enter the two digits: ");
+        int num1 = input.nextInt();
         int num2 = input.nextInt();
 
-       int tripled = num1*num1*num1;
-       int doubled = num2*num2;
+        int tripled = num1*num1*num1;
+        int doubled = num2*num2;
 
-       int multiple = tripled % doubled;
-       if (multiple ==0){
-           System.out.printf("This number %d when doubled is %d, is a multiple of first number %d, which when tripled, is %d", num2, doubled, num1, tripled);
-       }
-       if (multiple != 0){
-           System.out.printf("This number %d when doubled is %d, is not a multiple of the first number %d, which when tripled is %d", num2, doubled, num1, tripled);
-       }
+        int multiple = tripled % doubled;
+        if (multiple ==0){
+            System.out.printf("This number %d when doubled is %d, is a multiple of first number %d, %n which when tripled, is %d", num2, doubled, num1, tripled);
+        }
+        if (multiple != 0){
+            System.out.printf("This number %d when doubled is %d, is not a multiple of the first number %d, %n which when tripled is %d", num2, doubled, num1, tripled); //
+        }
     }
 }
