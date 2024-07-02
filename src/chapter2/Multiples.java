5427dfabc7eb4f83635e627bb739ddd887403b17
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-02 12:23:52 +0100

    Multiples

--- src/chapter2/Multiples.java
+++ src/chapter2/Multiples.java
@@ -7,20 +7,20 @@ public class Multiples {
         Scanner input = new Scanner(System.in);
 
         System.out.println("kindly enter the first number: ");
-        int number1 = input.nextInt();
+        int num1 = input.nextInt();
 
         System.out.println("kindly enter the second number: ");
-        int number2 = input.nextInt();
+        int num2 = input.nextInt();
 
-       int cube = number1*number1*number1;
-       int square = number2*number2;
+       int tripled = num1*num1*num1;
+       int doubled = num2*num2;
 
-       int multiple = cube % square;
+       int multiple = tripled % doubled;
        if (multiple ==0){
-           System.out.printf("This number %d when doubled is %d, is a multiple of first number %d, which when tripled, is %d", number2, square, number1, cube);
+           System.out.printf("This number %d when doubled is %d, is a multiple of first number %d, which when tripled, is %d", num2, doubled, num1, tripled);
        }
        if (multiple != 0){
-           System.out.printf("This number %d when doubled is %d, is not a multiple of the first number %d, which when tripled is %d", number2, square, number1, cube);
+           System.out.printf("This number %d when doubled is %d, is not a multiple of the first number %d, which when tripled is %d", num2, doubled, num1, tripled);
        }
     }
 }
