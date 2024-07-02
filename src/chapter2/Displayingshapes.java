50bdfd92f070bc4788945074111d3dad1f660ef0
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-02 13:39:45 +0100

    Displayingshapes

--- src/chapter2/Displayingshapes.java
+++ src/chapter2/Displayingshapes.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Displayingshapes {
     public static void main(String[] args) {
 
-        System.out.printf("%s       %s       %s       %s%n", "*********","***","*","*");
+       /* System.out.printf("%s       %s       %s       %s%n", "*********","***","*","*");
         System.out.printf("%s       %s     %s     %s    %s     %s %s%n", "*", "*", "*", "*", "***", "*", "*");
         System.out.printf("%s       %s    %s       %s  %s   %s   %s%n", "*", "*", "*", "*", "*****", "*", "*");
         System.out.printf("%s       %s    %s       %s    %s    %s     %s%n", "*", "*", "*", "*", "*","*", "*");
@@ -25,7 +25,11 @@ public class Displayingshapes {
         System.out.printf("%s       %s    %s       %s    %s    %s     %s%n", "*", "*", "*", "*", "*", "*", "*");
         System.out.printf("%s       %s    %s       %s    %s     %s   %s%n", "*", "*", "*", "*", "*","*", "*");
         System.out.printf("%s       %s     %s     %s     %s      %s %s%n", "*", "*", "*", "*", "*", "*", "*");
-        System.out.printf("%s       %s       %s       %s%n", "*********", "***","*", "*", "*", "*");
+        System.out.printf("%s       %s       %s       %s%n", "*********", "***","*", "*", "*", "*");  */
+
+
+     System.out.printf("* * * * * * * * %n%s%n%s%n%s%n%s%n%s%n%s%n%s%n", " * * * * * * * *",
+             "* * * * * * * * ", " * * * * * * * *", "* * * * * * * * ", " * * * * * * * * ", "* * * * * * * * ", " * * * * * * * * ");
 
     }
 }
