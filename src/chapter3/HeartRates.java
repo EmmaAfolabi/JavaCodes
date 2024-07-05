7b94af0340b65f857540201d18064b2f8fc81c61
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-05 12:17:30 +0100

    HeartRates

--- src/chapter3/HeartRates.java
+++ src/chapter3/HeartRates.java
@@ -6,7 +6,7 @@ public class HeartRates {
      String firstname = ("Emma");
      String lastname;
      int dateofbirth;
-     String month; String day; int yearofbirth;
+     String month; int day; int yearofbirth;
      int age;
 
     public String getFirstname() {
@@ -41,11 +41,11 @@ public class HeartRates {
         this.month = month;
     }
 
-    public String getDay() {
+    public int getDay() {
         return day;
     }
 
-    public void setDay(String day) {
+    public void setDay(int day) {
         this.day = day;
     }
 
@@ -58,7 +58,7 @@ public class HeartRates {
     }
 
 
-    public HeartRates(String firstname, String lastname, int dateofbirth, String month, String day, int yearofbirth) {
+    public HeartRates(String firstname, String lastname, int dateofbirth, String month, int day, int yearofbirth) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.dateofbirth = dateofbirth;
@@ -77,7 +77,7 @@ public class HeartRates {
     }
 
     public static void main(String[] args) {
-        HeartRates info = new HeartRates("emma", "ade", 19_12_4, "december", "17", 2001);
+        HeartRates info = new HeartRates("emma", "ade", 19_12_4, "december", 19, 2001);
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter your firstname: ");
@@ -87,7 +87,7 @@ public class HeartRates {
         info.lastname = input.next();
 
         System.out.println("Enter your day of birth: ");
-        info.day = input.next();
+        info.day = input.nextInt();
 
         System.out.println("Enter your month of birth: ");
         info.month = input.next();
@@ -95,8 +95,15 @@ public class HeartRates {
         System.out.println("Enter your year of birth: ");
         info.yearofbirth = input.nextInt();
 
-        System.out.printf(info.firstname, info.lastname );
+        int maximumheartrate = 220 - info.getAge();
+        double targetheartrate = (0.75) * maximumheartrate ;
 
+        System.out.printf("first name: %s%n " +
+                "lastname: %s%n",info.firstname, info.lastname );
+        System.out.printf("Date of birth : %d, %s, %d%n", info.getDay(), info.getMonth(), info.getYearofbirth());
+        System.out.printf("Your age: %d%n", info.getAge());
+        System.out.println("Your maximum heart rate in beats per minute is " + maximumheartrate);
+        System.out.println("Your target heart rate is " + targetheartrate);
 
 //        info.firstname = info.getFirstname();
 //        info.lastname = info.getLastname();
