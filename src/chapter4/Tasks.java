ea7832c95362da3609f74e7b1ba8b4c7e5ff3d65
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-16 13:34:49 +0100

    Tasks

---
+++ src/chapter4/Tasks.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Tasks {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int x = 1;
+
+        sum +=x;
+        System.out.printf("The sum is: %d%n", sum);
+    }
+}
