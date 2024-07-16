bc66615e4ee80e736d0bef4cd8e42f8332e12f23
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-16 14:04:50 +0100

    Sumofintegers

---
+++ src/chapter4/Sumofintegers.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Sumofintegers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int x = 1;
+
+        while ( x <= 10){
+            sum += x;
+            ++x;
+        }
+
+        System.out.printf("The sum is: %d%n", sum);
+    }
+}
