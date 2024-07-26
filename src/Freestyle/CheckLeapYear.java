0707e246e8acd4e12e5a5efd34d0ef64a74953b5
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-07-27 00:08:38 +0100

    CheckLeapYear

--- src/Freestyle/CheckLeapYear.java
+++ src/Freestyle/CheckLeapYear.java
@@ -6,17 +6,13 @@ public class CheckLeapYear {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
 
-            System.out.println("Enter the year: ");
+            System.out.println("Kindly input a year: ");
             int year = input.nextInt();
 
             if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-
                 System.out.println(year + " is a leap year. ");
-
             } else {
-
                 System.out.println(year + " is not a leap year. ");
-
             }
 
         }
