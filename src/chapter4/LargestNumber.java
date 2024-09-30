deadc7143641eba97fe4d629670f4696b72c2d3e
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-09-30 16:41:52 +0100

    LargestNumber

---
+++ src/chapter4/LargestNumber.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int counter = 0;
+        int number;
+        int largest = 0;
+    }
+}
