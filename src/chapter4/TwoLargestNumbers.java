5ea1a0e519ba385fcbd9088def6c2510337c4874
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-01 20:50:48 +0100

    TwoLargestNumbers

---
+++ src/chapter4/TwoLargestNumbers.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class TwoLargestNumbers {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int counter = 1;
+        int number;
+        int firstlargest;
+        int Secondlargest;
+
+    }
+}
