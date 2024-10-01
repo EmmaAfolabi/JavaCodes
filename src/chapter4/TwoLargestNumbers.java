2cf37345a28e09fdcdc7596a33a778c07ac67a23
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-02 00:06:57 +0100

    TwoLargestNumbers

--- src/chapter4/TwoLargestNumbers.java
+++ src/chapter4/TwoLargestNumbers.java
@@ -13,19 +13,21 @@ public class TwoLargestNumbers {
         int secondlargest = 0;
 
         System.out.println("Enter a series of ten numbers: ");
-        number = input.nextInt();
 
         while (counter <= 10){
+
+            System.out.printf("Enter number %d: ", counter);
+            number = input.nextInt();
+
             if (number > firstlargest){
+                secondlargest = firstlargest;
                 firstlargest = number;
             }
-            else
-                if (number > secondlargest){
+            else if (number > secondlargest){
                     secondlargest = number;
-                }
+            }
+            counter++;
         }
-        counter++;
-
 
     }
 }
