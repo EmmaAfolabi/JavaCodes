daf991d9304e23a84162147a27fa84474a9284cd
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-02 16:32:53 +0100

    ValidatingUserinput

--- src/chapter4/ValidatingUserinput.java
+++ src/chapter4/ValidatingUserinput.java
@@ -11,8 +11,11 @@ public class ValidatingUserinput {
             System.out.println("enter a value (1 or 2): ");
             userinput = input.nextInt();
 
+            if (userinput != 1 && userinput != 2){
+                System.out.println("Invalid input!! please enter 1 or 2");
+            }
+        } while (userinput != 1 && userinput != 2);
 
-        }
-
+        System.out.printf("You entered a valid value :%d%n", userinput);
     }
 }
