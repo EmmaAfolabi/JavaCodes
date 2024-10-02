e0756f5ea282a2c0c44c45c5c1235c552ac86eec
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-02 16:09:32 +0100

    ValidatingUserinput

---
+++ src/chapter4/ValidatingUserinput.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class ValidatingUserinput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int userinput;
+
+        do {
+            System.out.println("enter a value (1 or 2): ");
+            userinput = input.nextInt();
+
+
+        }
+
+    }
+}
