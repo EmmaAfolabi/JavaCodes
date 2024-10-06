acba36dd0549ddd3736e51aa432d464ebceebc4d
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-06 18:52:17 +0100

    Rightangledtriangle

--- src/chapter4/Rightangledtriangle.java
+++ src/chapter4/Rightangledtriangle.java
@@ -16,5 +16,11 @@ public class Rightangledtriangle {
                 System.out.println("Invalid input! Please enter a number between 1 and 10.");
             }
         }while (baselength < 1 || baselength > 10);
+
+        for (int i = 1; i <= baselength; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+        }
     }
 }
