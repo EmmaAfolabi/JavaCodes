df88b2209bed88a35a4f73caad49492f2b64fa70
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-07 16:57:46 +0100

    Palindrome

---
+++ src/chapter4/Palindrome.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Palindrome {
+    private int number;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
