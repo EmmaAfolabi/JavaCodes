de1589d765421d29e21698b8b498acb341173b17
EmmaAfolabi <daveemmanuel985@gmail.com>
2024-10-08 00:05:01 +0100

    Palindrome

--- src/chapter4/Palindrome.java
+++ src/chapter4/Palindrome.java
@@ -11,10 +11,19 @@ public class Palindrome {
     public void setNumber(int number) {
         this.number = number;
     }
+
     public boolean isPalindrome() {
         int reversedInt = 0;
         int InitialNum = number;
         int remainder;
         int tempNumber = number;
+
+        while (tempNumber > 0) {
+            remainder = tempNumber % 10;
+            reversedInt = reversedInt * 10 + remainder;
+            tempNumber = tempNumber / 10;
+        }
+
+        return reversedInt == InitialNum;
     }
 }
