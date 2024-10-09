eef8532309d4c2e391507caa7cd177f12b6e5f65
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-09 15:21:27 +0100

    Checkerboard

--- src/chapter4/Checkerboard.java
+++ src/chapter4/Checkerboard.java
@@ -2,22 +2,25 @@ package chapter4;
 
 public class Checkerboard {
     public static void main(String[] args) {
-         int row = 1;
+        int row = 1;
 
         while (row <= 8) {
 
            if (row % 2 != 0) {
                for (int column = 1; column <= 8; column++) {
-                   System.out.println("*");
+                   System.out.print("* ");
                }
            }
 
            else {
-               System.out.print("");
+               System.out.print(" ");
                for (int column = 1; column <= 8; column++) {
-                   System.out.println("*");
+                   System.out.print("* ");
                }
            }
+
+           System.out.println();
+           row++;
         }
     }
 }
