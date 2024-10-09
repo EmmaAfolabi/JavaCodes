bcbb73e85327cc0a77aa2aa6c05620200aea2a39
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-09 14:22:28 +0100

    Checkerboard

--- src/chapter4/Checkerboard.java
+++ src/chapter4/Checkerboard.java
@@ -4,6 +4,13 @@ public class Checkerboard {
     public static void main(String[] args) {
          int counter = 1;
 
+        while (counter <= 4) {
+            int row = 1;
 
+            while (row <= 8) {
+                System.out.print("*");
+                row++;
+            }
+        }
     }
 }
