ff82152f524129957b46a1d3fa3d6a166d8a6717
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-11 00:13:28 +0100

    Comparator

---
+++ src/chapter4/Comparator.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Comparator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int firstnumber = input.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int secondnumber = input.nextInt();
+
+
+    }
+}
