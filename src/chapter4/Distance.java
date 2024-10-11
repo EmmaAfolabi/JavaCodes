abd30aa78c315a41f44fe9bbcb6ada8a48c8e882
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-12 00:20:35 +0100

    Distance

---
+++ src/chapter4/Distance.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the x-coordinate of the first point: ");
+        int x1 = input.nextInt();
+
+        System.out.print("Enter the y-coordinate os the first point: ");
+        int y1 = input.nextInt();
+    }
+}
