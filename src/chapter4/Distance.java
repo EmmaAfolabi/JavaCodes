ad4eccf6f3aa85f4c211bcc8a1c6a3b2a54c7151
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-12 00:36:42 +0100

    Distance

--- src/chapter4/Distance.java
+++ src/chapter4/Distance.java
@@ -8,8 +8,20 @@ public class Distance {
 
         System.out.print("Enter the x-coordinate of the first point: ");
         int x1 = input.nextInt();
-
         System.out.print("Enter the y-coordinate os the first point: ");
         int y1 = input.nextInt();
+
+        System.out.print("Enter the x-coordinate of the second point: ");
+        int x2 = input.nextInt();
+        System.out.print("Enter the y-coordinate of the second point: ");
+        int y2 = input.nextInt();
+
+        if (x1 == x2) {
+            System.out.println("The points are on a vertical line i.e perpendicular to the x-axis.");
+        } else if (y1 == y2) {
+            System.out.println("The points are on a horizontal line i.e perpendicular to the y-axis.");
+        } else {
+            System.out.println("The points are not on a line perpendicular to either of the axis.");
+        }
     }
 }
