c0534aa4415eb57026b8c36386ea2a84388144cb
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-12 13:01:03 +0100

    Factorial

---
+++ src/chapter4/Factorial.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a non-negative integer: ");
+        int number = input.nextInt();
+
+
+    }
+}
