8df230381aa9c1337603d7f8421c54b8c5b2a7be
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-13 00:28:46 +0100

    constant

---
+++ src/chapter4/constant.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class constant {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms to calculate e: ");
+        int terms = input.nextInt();
+
+
+    }
+}
