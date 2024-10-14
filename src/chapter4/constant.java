1098c232ea8482bab11dff4b1f05700d8e7a6b4a
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-14 23:36:36 +0100

    constant

--- src/chapter4/constant.java
+++ src/chapter4/constant.java
@@ -9,6 +9,18 @@ public class constant {
         System.out.print("Enter the number of terms to calculate e: ");
         int terms = input.nextInt();
 
+        if (terms < 0) {
+            System.out.println("Error: The number of terms must be non-negative.");
+        } else {
+            double e = 1.0;
+            long factorial = 1;
 
+            for (int i = 1; i < terms; i++) {
+                factorial *= i;
+                e += 1.0 / factorial;
+            }
+
+
+        }
     }
 }
