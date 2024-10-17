808cd938029c1324016d519bc66dbcfcdf08929e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-17 23:52:04 +0100

    Cryptography

--- src/chapter4/Cryptography.java
+++ src/chapter4/Cryptography.java
@@ -1,8 +1,9 @@
 package chapter4;
 
 public class Cryptography {
-    int order =  0;
-    int place = 1;
-    String store = "";
-    int value1 =0, value2 =0, value3 =0, value4 =0;
+    public static void main(String[] args) {
+        int value1 =0, value2 =0, value3 =0, value4 =0;
+
+
+    }
 }
