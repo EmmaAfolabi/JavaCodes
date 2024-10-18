3e1fc474fa7e692313052f49dfb09a10147c89e0
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-19 00:43:12 +0100

    Cryptography

--- src/chapter4/Cryptography.java
+++ src/chapter4/Cryptography.java
@@ -4,6 +4,12 @@ public class Cryptography {
     public String encryptNumber(int digits) {
         int value1 =0, value2 =0, value3 =0, value4 =0;
 
-
+        value4 = (digits % 10 + 7) % 10;
+        digits /= 10;
+        value3 = (digits % 10 + 7) % 10;
+        digits /= 10;
+        value2 = (digits % 10 + 7) % 10;
+        digits /= 10;
+        value1 = ( digits % 10 + 7) % 10;
     }
 }
