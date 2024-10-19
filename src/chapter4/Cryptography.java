06ae97bf28706a8417875b62fdec5d7f7e7069d0
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-19 17:02:32 +0100

    Cryptography

--- src/chapter4/Cryptography.java
+++ src/chapter4/Cryptography.java
@@ -1,15 +1,28 @@
 package chapter4;
 
+import java.util.Scanner;
+
 public class Cryptography {
     public String encryptNumber(int digits) {
         int value1 =0, value2 =0, value3 =0, value4 =0;
 
-        value4 = (digits % 10 + 7) % 10;
+        value4 = ((digits % 10) + 7) % 10;
         digits /= 10;
-        value3 = (digits % 10 + 7) % 10;
+        value3 = ((digits % 10) + 7) % 10;
         digits /= 10;
-        value2 = (digits % 10 + 7) % 10;
+        value2 = ((digits % 10) + 7) % 10;
         digits /= 10;
-        value1 = ( digits % 10 + 7) % 10;
+        value1 = ((digits % 10) + 7) % 10;
+
+        String encrypted = value3 + "" + value4 + "" + value1 + "" + value2;
+        return encrypted;
+    }
+
+    public static void main(String[] args) {
+        Cryptography message = new Cryptography();
+
+        Scanner input = new Scanner(System.in);
+
+
     }
 }
