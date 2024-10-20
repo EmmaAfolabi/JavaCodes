7095ba378dba6615793ea12a27b5226fc8d54af9
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-20 23:57:25 +0100

    WorldPopulation

---
+++ src/chapter4/WorldPopulation.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class WorldPopulation {
+
+    public void calculateWorldGrowth() {
+
+        int yearCounter = 1;
+        double rate = 0.0107;
+        double populationIncrease = 0;
+        double currentPopulation = 7346235000.00;
+
+
+    }
+}
