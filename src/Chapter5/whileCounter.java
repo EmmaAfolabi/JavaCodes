3202d67d8e9ab8fd8f103950ca3ef4f0be509dc9
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-26 12:03:59 +0100

    whileCounter

--- src/Chapter5/whileCounter.java
+++ src/Chapter5/whileCounter.java
@@ -4,6 +4,8 @@ public class whileCounter {
     public static void main(String[] args) {
         int counter = 1;
 
-
+        while (counter <= 10) {
+            System.out.printf("%d", counter);
+        }
     }
 }
