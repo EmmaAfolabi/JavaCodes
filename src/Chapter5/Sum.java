92acc2c7a1ef0dbefc77a66b1cd404e46777a486
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-27 21:33:01 +0100

    Sum

--- src/Chapter5/Sum.java
+++ src/Chapter5/Sum.java
@@ -4,5 +4,10 @@ public class Sum {
     public static void main(String[] args) {
 
         int total = 0;
+
+
+        for (int number = 2; number <= 20; number += 2) {
+            total += number;
+        }
     }
 }
