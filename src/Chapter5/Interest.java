f1ba4e2055ca1b833c4a19a765e00c85dba1cafb
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-28 00:34:55 +0100

    Interest

---
+++ src/Chapter5/Interest.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class Interest {
+    public static void main(String[] args) {
+
+        double principal = 1000.0;
+        double rate = 0.05;
+
+
+        System.out.printf("%s%20%s%n", "Year", "Amount on deposit");
+    }
+}
