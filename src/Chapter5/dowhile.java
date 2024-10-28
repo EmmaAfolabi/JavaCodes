e486ed75fe4c341629eada2d55137fd954a793a1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-28 11:17:33 +0100

    dowhile

---
+++ src/Chapter5/dowhile.java
@@ -0,0 +1,13 @@
+package Chapter5;
+
+public class dowhile {
+
+    public static void main(String[] args) {
+        int counter = 1;
+
+        do {
+            System.out.printf("%d  ", counter);
+            ++counter;
+        } while (counter <= 10);
+    }
+}
