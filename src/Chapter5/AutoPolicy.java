8816f69faa431a00591d99fca0ade084c15da13d
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-29 15:17:17 +0100

    AutoPolicy

--- src/Chapter5/AutoPolicy.java
+++ src/Chapter5/AutoPolicy.java
@@ -2,6 +2,24 @@ package Chapter5;
 //class that represents an auto insurance policy
 public class AutoPolicy {
     private int accountNumber;     //policy account number
-    private String MakeandModel;  //car that the policy applies to
+    private String makeAndModel;  //car that the policy applies to
     private String state;        //two-letter state abbreviation
+
+    //constructor
+    public AutoPolicy(int accountNumber, String makeAndModel,
+                      String state) {
+        this.accountNumber = accountNumber;
+        this.makeAndModel = makeAndModel;
+        this.state = state;
+    }
+
+    //sets the accountNumber
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    //returns the accountNumber
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
