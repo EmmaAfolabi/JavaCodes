982d453edcfeab3f3541c7a81fb4ce394fdc3dd5
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-10-31 23:43:58 +0100

    ContinueTest

---
+++ src/Chapter5/ContinueTest.java
@@ -0,0 +1,11 @@
+package Chapter5;
+
+public class ContinueTest {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 10; count++) {
+            if (count == 5) {
+                continue;
+            }
+        }
+    }
+}
