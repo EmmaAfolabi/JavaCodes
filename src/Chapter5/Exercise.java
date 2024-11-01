5544c4796444f311e09a3638a3b8fffb0993c984
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 00:36:04 +0100

    Exercise

--- src/Chapter5/Exercise.java
+++ src/Chapter5/Exercise.java
@@ -5,6 +5,13 @@ public class Exercise {
         int i = 1;
         for (i = 1; i <= 20; i++) {
             System.out.print(i);
+
+            if (i % 5 == 0) {
+                System.out.println();
+            }
+            else {
+                System.out.print('\t');
+            }
         }
     }
 }
