59a1a83d2bebfbbe8eb609ec32be3ba8cd6ac0d1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 00:15:39 +0100

    Exercise

---
+++ src/Chapter5/Exercise.java
@@ -0,0 +1,11 @@
+package Chapter5;
+
+public class Exercise {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 20) {
+            System.out.print(i);
+        }
+git
+    }
+}
