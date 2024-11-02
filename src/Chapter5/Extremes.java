55455f604b93c1680e2504d4ce3bc91a9891b2f1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 19:42:42 +0100

    Extremes

---
+++ src/Chapter5/Extremes.java
@@ -0,0 +1,16 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class Extremes {
+    public void findMinMaxSum() {
+        int i = 1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of values: ");
+        int counter = input.nextInt();
+
+        System.out.println("Enter the values: ");
+        int next = input.nextInt();
+    }
+}
