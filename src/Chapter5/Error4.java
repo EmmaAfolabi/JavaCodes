33896a0ccc9323c43b6be501d3998996e163ada4
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 12:53:37 +0100

    Error4

---
+++ src/Chapter5/Error4.java
@@ -0,0 +1,11 @@
+package Chapter5;
+
+public class Error4 {
+    public static void main(String[] args) {
+        int counter = 0;
+        do {
+            System.out.println(counter);
+            counter += 2;
+        } while (counter <= 50);
+    }
+}
