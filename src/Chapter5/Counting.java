24d6a4ed0b1188ef3d1e5f1f97acb3228e9651d4
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 14:10:46 +0100

    Counting

--- src/Chapter5/Counting.java
+++ src/Chapter5/Counting.java
@@ -9,6 +9,7 @@ public class Counting {
         for (int i = 1; i < 3; i++) {
             for (int j = 1; j < 5; j++)
                 System.out.println('*');
+            System.out.println("\n#####");
         }
     }
 }
