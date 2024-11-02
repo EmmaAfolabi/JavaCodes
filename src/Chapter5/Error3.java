252c1e56324f5b4cc219af698bedb5acabe73770
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-02 12:44:49 +0100

    Error3

---
+++ src/Chapter5/Error3.java
@@ -0,0 +1,10 @@
+package Chapter5;
+
+public class Error3 {
+    public static void main(String[] args) {
+
+        for (int i = 19; i > 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
