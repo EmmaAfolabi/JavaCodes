a574f6c18f559dafadcb3befb8e83e818ae53bf0
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-03 00:48:13 +0100

    Divisible

--- src/Chapter5/Divisible.java
+++ src/Chapter5/Divisible.java
@@ -6,7 +6,11 @@ public class Divisible {
         int sum = 0;
 
         for (int i = 1; i <= 30; i++) {
-
+            if (i % 3 == 0) {
+                sum += i;
+            }
         }
+
+        System.out.printf("Sum of integers between 1 and 30 that are divisible by 3 is: %d%n", sum);
     }
 }
