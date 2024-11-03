58f8da5083f6a3944e2129a95a4c82efab8984a0
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-03 18:54:39 +0100

    CompoundInterest

--- src/Chapter5/CompoundInterest.java
+++ src/Chapter5/CompoundInterest.java
@@ -1,6 +1,10 @@
 package Chapter5;
 
 public class CompoundInterest {
-    double principal = 1000.00;
 
+    public void calculateYearlyInterest() {
+        double principal = 1000.00;
+
+
+    }
 }
