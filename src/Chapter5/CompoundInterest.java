95beab6d6b430b7f7368c9ac62498301972c614f
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-03 20:28:02 +0100

    CompoundInterest

--- src/Chapter5/CompoundInterest.java
+++ src/Chapter5/CompoundInterest.java
@@ -5,6 +5,9 @@ public class CompoundInterest {
     public void calculateYearlyInterest() {
         double principal = 1000.00;
 
-
+        for (double rate = 0.05; rate <= 10; rate += 0.01) {
+            System.out.printf("Interest rate: %.2f%%%n", rate * 100);
+            System.out.printf("%-4s%20s%n", "Year", "Amount on deposit");
+        }
     }
 }
