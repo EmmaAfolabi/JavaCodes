b4b28b2a1eb8a1ea8738f83e72e31ebceb22b805
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 08:59:09 +0100

    Triangle

--- src/Chapter5/Triangle.java
+++ src/Chapter5/Triangle.java
@@ -5,7 +5,19 @@ public class Triangle {
     public static void main(String[] args) {
 
         //Triangle A
+        System.out.println("Triangle A");
         for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //Triangle B
+        System.out.println("Triangle B");
+        for (int i = 10; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print('*');
             }
