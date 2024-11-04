c7149975501b035b05446adfd76b21d80e0bf799
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 09:58:30 +0100

    Triangle

--- src/Chapter5/Triangle.java
+++ src/Chapter5/Triangle.java
@@ -25,5 +25,32 @@ public class Triangle {
         }
 
         System.out.println();
+
+
+        //Triangle C
+        System.out.println("Triangle D");
+        for (int i = 10; i >= 1; i--) {
+            for (int j = 10; j > i; j--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        //Triangle D
+        System.out.println("Triangle C");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 10; j > i; j--) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        System.out.println();
     }
 }
