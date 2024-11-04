0bf413541dc57ab76db7f8390bd0535ebc6380d2
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 16:43:07 +0100

    AssumeA

--- src/Chapter5/AssumeA.java
+++ src/Chapter5/AssumeA.java
@@ -8,5 +8,15 @@ public class AssumeA {
         System.out.println(i == 2);
 
         System.out.println(j == 5);
+
+        System.out.println((i >= 0) && (j <= 3));
+
+        System.out.println((m <= 100) & (k <= m));
+
+        System.out.println((j >= i) || (k != m));
+
+        System.out.println((k + i < j) | (4 - j >= k));
+
+        System.out.println(!(k > j));
     }
 }
