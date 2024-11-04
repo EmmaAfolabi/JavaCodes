b2b924a4cd2e96c2fb6ecf9af80c6169495282b4
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 16:08:02 +0100

    ModifiedCompoundInterest

---
+++ src/Chapter5/ModifiedCompoundInterest.java
@@ -0,0 +1,20 @@
+package Chapter5;
+
+public class ModifiedCompoundInterest {
+
+    public void calculateInterest()
+    {
+        int principal = 1000 * 100;
+        double rate = 1 + 0.05;
+        int years = 10;
+        int dollars = 0;
+        int cents = 0;
+        double amount = 0;
+
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+        for (int i = 1; i <= years; i++) {
+
+
+        }
+    }
+}
