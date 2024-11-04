a25863a93e736b99c261b10107883f757deda8ca
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 10:14:05 +0100

    BarChart

--- src/Chapter5/BarChart.java
+++ src/Chapter5/BarChart.java
@@ -8,7 +8,16 @@ public class BarChart {
         int value = 0;
         while (counter <= 5)
         {
-
+            if (counter == 1)
+                value = one;
+            else if (counter == 2)
+                value = two;
+            else if (counter == 3)
+                value = three;
+            else if (counter == 4)
+                value = four;
+            else if (counter == 5)
+                value = five;
         }
     }
 }
