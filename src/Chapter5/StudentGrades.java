89041c0a0875b8f69b42ce7bed4d7301c145cb3f
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 13:54:42 +0100

    StudentGrades

--- src/Chapter5/StudentGrades.java
+++ src/Chapter5/StudentGrades.java
@@ -12,5 +12,32 @@ public class StudentGrades {
         int countD = 0;
         String studentName;
         char grade;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Enter student name:");
+            studentName = input.next();
+
+            System.out.println("Enter student grade (A, B, C, D):");
+            grade = input.next().charAt(0);
+
+            switch (grade) {
+                case 'A':
+                    countA++;
+                    break;
+                case 'B':
+                    countB++;
+                    break;
+                case 'C':
+                    countC++;
+                    break;
+                case 'D':
+                    countD++;
+                    break;
+                default:
+                    System.out.println("Invalid grade entered. Please enter a grade from A, B, C, or D.");
+                    i--;
+                    break;
+            }
+        }
     }
 }
