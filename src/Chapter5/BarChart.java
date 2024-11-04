aaab80271416a7d542b5fab5854e33d978b4156e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 10:09:13 +0100

    BarChart

---
+++ src/Chapter5/BarChart.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class BarChart {
+
+    public void printBarChart(int one, int two, int three, int four, int five)
+    {
+        int counter = 1;
+        int value = 0;
+        while (counter <= 5)
+        {
+
+        }
+    }
+}
