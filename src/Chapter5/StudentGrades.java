ba94b4dc84a1b67f4f92d7256d9d32314adc7174
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 12:53:57 +0100

    StudentGrades

---
+++ src/Chapter5/StudentGrades.java
@@ -0,0 +1,16 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class StudentGrades {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+        int countD = 0;
+        String studentName;
+        char grade;
+    }
+}
