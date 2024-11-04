0ac0314e874214661c61c4e555984527cb004fe2
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-04 16:30:45 +0100

    AssumeA

---
+++ src/Chapter5/AssumeA.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class AssumeA {
+    public static void main(String[] args) {
+
+        int i =2, j = 3, k = 2, m = 2;
+
+        System.out.println(i == 2);
+
+        System.out.println(j == 5);
+    }
+}
