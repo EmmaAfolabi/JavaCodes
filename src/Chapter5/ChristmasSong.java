8ae8f2c57438da82eb1d0232f95e6ca3bd129b12
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-05 16:15:01 +0100

    ChristmasSong

--- src/Chapter5/ChristmasSong.java
+++ src/Chapter5/ChristmasSong.java
@@ -44,7 +44,45 @@ public class ChristmasSong {
                     System.out.println("On the twelfth day of Christmas my true love sent to me");
             }
 
-
+            switch (days) {
+                case 12:
+                    System.out.println("twelve drummers drumming");
+                    break;
+                case 11:
+                    System.out.println("eleven pipers piping");
+                    break;
+                case 10:
+                    System.out.println("ten lords a-leaping");
+                    break;
+                case 9:
+                    System.out.println("nine ladies dancing");
+                    break;
+                case 8:
+                    System.out.println("eight maids a-milking");
+                    break;
+                case 7:
+                    System.out.println("seven swans a-swimming");
+                    break;
+                case 6:
+                    System.out.println("six geese a-laying");
+                    break;
+                case 5:
+                    System.out.println("five gold rings");
+                    break;
+                case 4:
+                    System.out.println("four calling birds");
+                    break;
+                case 3:
+                    System.out.println("Three French hens");
+                    break;
+                case 2:
+                    System.out.println("Two turtle doves");
+                    break;
+                case 1:
+                    System.out.println("And a partridge in a pear tree");
+            }
+            System.out.println();
+            days++;
         }
     }
 }
