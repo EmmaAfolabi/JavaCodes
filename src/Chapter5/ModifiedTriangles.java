9cc08b4f3017cb35fd4fec3d0d58d46f103ad4c9
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-05 09:49:00 +0100

    ModifiedTriangles

---
+++ src/Chapter5/ModifiedTriangles.java
@@ -0,0 +1,9 @@
+package Chapter5;
+
+public class ModifiedTriangles {
+    public static void main(String[] args) {
+
+        System.out.printf("%s%15s%23s%17s%n", "(a)", "(b)", "(c)", "(d)");
+        int A = 1, B = 10, E = 5, F = 15;
+    }
+}
