cb2bdd2621d0086866827d1e35c04d2ababa0bd3
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-05 15:28:15 +0100

    ChristmasSong

---
+++ src/Chapter5/ChristmasSong.java
@@ -0,0 +1,15 @@
+package Chapter5;
+
+public class ChristmasSong {
+
+    public void displayChristmasSong() {
+        int days = 1;
+
+        while (days <= 12) {
+            switch (days) {
+                case 1:
+                    System.out.println("On the first day of Christmas my true love sent to me");
+            }
+        }
+    }
+}
