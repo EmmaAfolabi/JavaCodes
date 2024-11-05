8914abd2f5056597e2a86afc6d7160b836f0c54a
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-05 10:00:13 +0100

    ModifiedTriangles

--- src/Chapter5/ModifiedTriangles.java
+++ src/Chapter5/ModifiedTriangles.java
@@ -5,5 +5,23 @@ public class ModifiedTriangles {
 
         System.out.printf("%s%15s%23s%17s%n", "(a)", "(b)", "(c)", "(d)");
         int A = 1, B = 10, E = 5, F = 15;
+        for (int t = 1; t <= 10; t++) {
+            //TriangleA
+            for (int n = A; n > 0; n--) {
+                System.out.print("*");
+            }
+            //spaces
+            for (int a = F; a > 0; a--) {
+                System.out.print(" ");
+            }
+            //TriangleB
+            for (int b = B; b > 0; b--) {
+                System.out.print("*");
+            }
+            //spaces
+            for (int a = E; a > 0; a--) {
+                System.out.print(" ");
+            }
+        }
     }
 }
