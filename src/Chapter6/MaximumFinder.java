47fc8fd08380cc258ad27089a3cd1b6a77ca7df3
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-06 14:47:34 +0100

    MaximumFinder

--- src/Chapter6/MaximumFinder.java
+++ src/Chapter6/MaximumFinder.java
@@ -13,5 +13,28 @@ public class MaximumFinder {
         double number1 = input.nextDouble();
         double number2 = input.nextDouble();
         double number3 = input.nextDouble();
+
+        //determine the maximum value
+        double result = maximum(number1, number2, number3);
+
+        // display maximum value
+        System.out.printf("Maximum is: %.2f%n", result);
+    }
+
+    // returns the maximum of it's three double parameters
+    public static double maximum(double x, double y, double z) {
+        double maximumValue = x; // assume x to be the largest to start
+
+        // determine whether y is greater than maximumValue
+        if (y > maximumValue) {
+            maximumValue = y;
+        }
+
+        // determine whether z is greater than maximumvalue
+        if (z > maximumValue) {
+            maximumValue = z;
+        }
+
+        return maximumValue;
     }
 }
