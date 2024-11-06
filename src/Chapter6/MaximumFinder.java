2cf2ebe30f2120ada77e472ec50482f7d0ba7adc
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-06 13:48:53 +0100

    MaximumFinder

---
+++ src/Chapter6/MaximumFinder.java
@@ -0,0 +1,17 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class MaximumFinder {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Input the three floating-point values
+        System.out.print(
+                "Enter three floating-point values separated by spaces: ");
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+        double number3 = input.nextDouble();
+    }
+}
