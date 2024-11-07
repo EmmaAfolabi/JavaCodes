d96232cd98b8f94faf0e3d683805b253e1a20fbc
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-07 15:42:17 +0100

    RandomIntegers

---
+++ src/Chapter6/RandomIntegers.java
@@ -0,0 +1,11 @@
+package Chapter6;
+
+import java.security.SecureRandom;
+
+public class RandomIntegers {
+    public static void main(String[] args) {
+        // randomNumbers object will produce secure random numbers
+        SecureRandom randomNumbers = new SecureRandom();
+
+    }
+}
