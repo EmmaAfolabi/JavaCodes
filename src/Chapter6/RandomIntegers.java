c847b38e89181a56257bff3a0a6320a1ef973a69
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-08 08:58:45 +0100

    RandomIntegers.java

--- src/Chapter6/RandomIntegers.java
+++ src/Chapter6/RandomIntegers.java
@@ -12,7 +12,12 @@ public class RandomIntegers {
             // picks random integer from 1 to 6
             int face = 1 + randomNumbers.nextInt(6);
 
-            System.out.printf("%d  ", face);
+            System.out.printf("%d  ", face); // display generated value
+
+            // if counter is divisible by 5, start a new line of output
+            if (counter % 5 == 0) {
+                System.out.println();
+            }
         }
     }
 }
