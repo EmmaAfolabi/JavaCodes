e4515cbc3bcbd5536a6f30b19ed8631297781f48
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-08 09:10:39 +0100

    RollDie.java

---
+++ src/Chapter6/RollDie.java
@@ -0,0 +1,17 @@
+package Chapter6;
+
+import java.security.SecureRandom;
+
+public class RollDie {
+    public static void main(String[] args) {
+        // randomNumbers object will produce secure random numbers
+        SecureRandom randomNumbers = new SecureRandom();
+
+        int frequency1 = 0;  //  count of 1s rolled
+        int frequency2 = 0;  //  count of 2s rolled
+        int frequency3 = 0;  //  count of 3s rolled
+        int frequency4 = 0;  //  count of 4s rolled
+        int frequency5 = 0;  //  count of 5s rolled
+        int frequency6 = 0;  //  count of 6s rolled
+    }
+}
