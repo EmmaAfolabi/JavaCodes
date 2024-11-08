698a588a229dc6caf6801831be71a353f5082cf6
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-08 09:24:51 +0100

    RollDie.java

--- src/Chapter6/RollDie.java
+++ src/Chapter6/RollDie.java
@@ -13,5 +13,32 @@ public class RollDie {
         int frequency4 = 0;  //  count of 4s rolled
         int frequency5 = 0;  //  count of 5s rolled
         int frequency6 = 0;  //  count of 6s rolled
+
+        // tally counts for 60,000,000 rolls of a die
+        for (int roll = 1; roll <= 60_000_000; roll++) {
+            int face = 1 + randomNumbers.nextInt(6); // number from 1 to 6
+
+            // use face value 1-6 to determine which counter to increment
+            switch (face) {
+                case 1:
+                    ++frequency1;
+                    break;
+                case 2:
+                    ++frequency2;
+                    break;
+                case 3:
+                    ++frequency3;
+                    break;
+                case 4:
+                    ++frequency4;
+                    break;
+                case 5:
+                    ++frequency5;
+                    break;
+                case 6:
+                    ++frequency6;
+                    break;
+            }
+        }
     }
 }
