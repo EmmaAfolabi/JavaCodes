74b35a278773453b9cddea47524680dd22440979
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-08 17:00:05 +0100

    Craps.java

---
+++ src/Chapter6/Craps.java
@@ -0,0 +1,13 @@
+package Chapter6;
+
+import java.security.SecureRandom;
+
+public class Craps {
+    // create secure random number generator for use in method rollDice
+    private static final SecureRandom randomNumbers = new SecureRandom();
+
+    // enum type with constants that represents the game status
+    private enum Status {CONTINUE, WON, LOST};
+
+    // constants that represents common roles of the dice
+}
