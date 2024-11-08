4c91c9f57a421fa4e1370b1ee623fa112d2df953
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-08 10:06:04 +0100

    RollDie.java

--- src/Chapter6/RollDie.java
+++ src/Chapter6/RollDie.java
@@ -21,24 +21,29 @@ public class RollDie {
             // use face value 1-6 to determine which counter to increment
             switch (face) {
                 case 1:
-                    ++frequency1;
+                    ++frequency1;  // increment the 1s counter
                     break;
                 case 2:
-                    ++frequency2;
+                    ++frequency2; //  increment the 2s counter
                     break;
                 case 3:
-                    ++frequency3;
+                    ++frequency3; //  increment the 3s counter
                     break;
                 case 4:
-                    ++frequency4;
+                    ++frequency4; //  increment the 4s counter
                     break;
                 case 5:
-                    ++frequency5;
+                    ++frequency5; //  increment the 5s counter
                     break;
                 case 6:
-                    ++frequency6;
+                    ++frequency6; //  increment the 6s counter
                     break;
             }
         }
+
+        System.out.println("Face\tFrequency");  // output headers
+        System.out.printf("1\t%d%n2\t%d%n3\t%d%n4\t%d%n5\t%d%n6\t%d%n",
+                frequency1, frequency2, frequency3, frequency4,
+                frequency5, frequency6);
     }
 }
