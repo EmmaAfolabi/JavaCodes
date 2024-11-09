1dbb408a7e9fc6b4d5da6d92310793b60e673bd1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-09 01:18:24 +0100

    Craps.java

--- src/Chapter6/Craps.java
+++ src/Chapter6/Craps.java
@@ -67,6 +67,15 @@ public class Craps {
 
     // roll dice, calculate sum and display results
     public static int rollDice() {
+        // pick random die values
+        int die1 = 1 + randomNumbers.nextInt(6);  // first die roll
+        int die2 = 1 + randomNumbers.nextInt(6); // second die roll
 
+        int sum = die1 + die2;  // sum of die values
+
+        // display results of this roll
+        System.out.printf("Player rolled %d  + %d = %d%n", die1, die2, sum);
+
+        return sum;
     }
 }
