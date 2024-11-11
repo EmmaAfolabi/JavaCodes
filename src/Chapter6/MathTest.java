a396c9da255c1f49f959f8517f340138baffb293
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-11 12:32:44 +0100

    Scope.java

---
+++ src/Chapter6/MathTest.java
@@ -0,0 +1,10 @@
+package Chapter6;
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.printf("Math.abs(23.7) = %f%n", Math.abs(23.7));
+        System.out.printf("Math.abs(0.0) = %f%n", Math.abs(0.0));
+        System.out.printf("Math.abs(-23.7) = %f%n", Math.abs(-23.7));
+        System.out.printf("Math.ceil(9.2) = %f%n", Math.abs(9.2));
+    }
+}
