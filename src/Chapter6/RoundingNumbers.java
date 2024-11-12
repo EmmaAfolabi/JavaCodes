ff099ea0651f5f82cb73cc9e3568222d3f7aa372
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-12 15:13:20 +0100

    Scope.java

---
+++ src/Chapter6/RoundingNumbers.java
@@ -0,0 +1,14 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class RoundingNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter value to be rounded");
+        double x = input.nextDouble();
+    }
+}
