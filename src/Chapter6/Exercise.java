3af6c899191e6aebe2c21b5ced55d6729d286513
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-12 13:07:02 +0100

    Scope.java

--- src/Chapter6/Exercise.java
+++ src/Chapter6/Exercise.java
@@ -6,6 +6,8 @@ public class Exercise {
         System.out.printf("Math.floor(5 + 2.5) = %f%n", Math.floor(5 + 2.5));
         System.out.printf("Math.abs(9) + Math.ceil(2.2) = %f%n", Math.abs(9) + Math.ceil(2.2));
         System.out.printf("Math.ceil(-5.2) = %f%n", Math.ceil(-5.2));
-
+        System.out.printf("Math.abs(-5) + Math.abs(4) = %d%n", Math.abs(-5) + Math.abs(4));
+        System.out.printf("Math.ceil(-6.4) - Math.floor(5.2) = %f%n", Math.ceil(-6.4) - Math.floor(5.2));
+        System.out.printf("Math.ceil(-Math.abs(-3 + Math.floor(-2.5)) = %f%n", Math.ceil(-Math.abs(-3 + Math.floor(-2.5))));
     }
 }
