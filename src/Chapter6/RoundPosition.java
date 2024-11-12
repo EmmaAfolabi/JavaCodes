ce766337a83bb236c32f499c96b4c2f7abb790aa
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-12 18:15:33 +0100

    Scope.java

--- src/Chapter6/RoundPosition.java
+++ src/Chapter6/RoundPosition.java
@@ -1,5 +1,7 @@
 package Chapter6;
 
+import java.util.Scanner;
+
 public class RoundPosition {
 
     public static double roundToInteger(double number) {
@@ -25,5 +27,23 @@ public class RoundPosition {
         double y = Math.floor(number * 1000 + 0.5) / 1000;
 
         return y;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter value: ");
+        double y = input.nextDouble();
+
+        System.out.println("Original value is: " + y);
+
+        System.out.println("value rounded to the nearest integer is: " + roundToInteger(y));
+
+        System.out.println("value rounded to the nearest tenth is: " + roundToTenths(y));
+
+        System.out.println("value rounded to the nearest hundreths is: " + roundToHundredths(y));
+
+        System.out.println();
     }
 }
