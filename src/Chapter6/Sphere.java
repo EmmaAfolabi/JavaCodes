90f6e4bf7ddf72817db28128a7debea012167fc8
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-12 10:39:17 +0100

    Scope.java

---
+++ src/Chapter6/Sphere.java
@@ -0,0 +1,13 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class Sphere {
+    // obtain radius from user and display volume of sphere
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter radius of sphere: ");
+        double radius = input.nextDouble();
+    }
+}
