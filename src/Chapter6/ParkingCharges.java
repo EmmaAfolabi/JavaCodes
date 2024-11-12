f8cf5353630566706d75c4d17193955803449716
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-12 14:35:52 +0100

    Scope.java

--- src/Chapter6/ParkingCharges.java
+++ src/Chapter6/ParkingCharges.java
@@ -1,5 +1,7 @@
 package Chapter6;
 
+import java.util.Scanner;
+
 public class ParkingCharges {
 
     // method to calculate charges based on hours parked
@@ -16,4 +18,28 @@ public class ParkingCharges {
         }
         return charges;
     }
+
+    // method to calculate the total receipts from multiple customers
+    public static double calculateReceipts() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter total number of receipts: ");
+        int count = input.nextInt();
+        int hours;
+        double receiptTotal = 0;
+
+        for (int d = 1; d <= count; d++) {
+            System.out.println("Enter hours parked: ");
+            hours = input.nextInt();
+
+            double charge = calculateCharges(hours);
+            System.out.printf("Customer charge is: %.2f%n", charge);
+            receiptTotal += charge;
+        }
+
+        input.close();
+        return receiptTotal;
+    }
+
+    // main method to execute the program
 }
