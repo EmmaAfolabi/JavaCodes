0d3b053edc7c1ddf031847a76f9a53f4c0a82f2a
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-13 17:40:38 +0100

    Expponentiation

--- src/Chapter6/Hypotenuse.java
+++ src/Chapter6/Hypotenuse.java
@@ -10,5 +10,13 @@ public class Hypotenuse {
         System.out.println("Enter two sides of a right angle triangle separated by spaces: ");
         double sideA = in.nextDouble();
         double sideB = in.nextDouble();
+
+        System.out.println("Hypotenuse is: " + hypotenuse(sideA, sideB));
+        in.close();
+    }
+
+    public static double hypotenuse(double side1, double side2) {
+        double hypotenuseSquared = Math.pow(side1, 2) + Math.pow(side2, 2);
+        return Math.sqrt(hypotenuseSquared);
     }
 }
