1be913350a83cb736bd6e163260b7ff060290372
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-13 17:35:47 +0100

    Expponentiation

---
+++ src/Chapter6/Hypotenuse.java
@@ -0,0 +1,14 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class Hypotenuse {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two sides of a right angle triangle separated by spaces: ");
+        double sideA = in.nextDouble();
+        double sideB = in.nextDouble();
+    }
+}
