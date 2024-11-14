00dc74a34586f80cde27578dbdd706eac209e7cb
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-14 10:05:33 +0100

    Divisible.java

--- src/Chapter6/Divisible.java
+++ src/Chapter6/Divisible.java
@@ -1,5 +1,7 @@
 package Chapter6;
 
+import java.util.Scanner;
+
 public class Divisible {
     // method to check if a number is divisible by 5
     public static boolean isDivisible(int number) {
@@ -7,6 +9,17 @@ public class Divisible {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 10 integers to check if it's divisible by 5: ");
 
+        for (int i = 0; i < 10; i++) {
+            int input = scanner.nextInt();
+            if (isDivisible(input)) {
+                System.out.printf("%d is divisible by 5.%n", input);
+            } else {
+                System.out.printf("%d is not divisible by 5.%n", input);
+            }
+        }
+        scanner.close();
     }
 }
