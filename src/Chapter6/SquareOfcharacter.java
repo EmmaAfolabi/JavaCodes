1ea0f084670418824013f01ccad0d2990b03252f
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-14 10:40:56 +0100

    SquareOfcharacter.java

--- src/Chapter6/SquareOfcharacter.java
+++ src/Chapter6/SquareOfcharacter.java
@@ -1,5 +1,7 @@
 package Chapter6;
 
+import java.util.Scanner;
+
 public class SquareOfcharacter {
 
     public static void displayCharacter(char fill, int size) {
@@ -11,5 +13,12 @@ public class SquareOfcharacter {
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter shape fill");
     }
 }
