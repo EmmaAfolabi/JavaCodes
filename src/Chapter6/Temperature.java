b1d3b375b2ac977b2ef39f54f6fc91ebb67f9519
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-15 00:45:59 +0100

    Temperature.java

--- src/Chapter6/Temperature.java
+++ src/Chapter6/Temperature.java
@@ -1,5 +1,7 @@
 package Chapter6;
 
+import java.util.Scanner;
+
 public class Temperature {
 
     // Method kelvin returns the kelvin equivalent of a celcius temperature
@@ -13,6 +15,22 @@ public class Temperature {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter temperature type ('k' for kelvin, 'c' for Celsius): ");
+        char type = input.next().charAt(0);
+
+        System.out.println("Enter the temperature value: ");
+        double temp = input.nextDouble();
+
+        if (type == 'k') {
+            System.out.printf("Celsius equivalent is: %.2f~C\n", celsiusFromKelvin(temp));
+        } else if (type == 'c') {
+            System.out.printf("kelvin temperature is: %.2fk\n", kelvin(temp));
+        } else {
+            System.out.println("Invalid temperature type.");
+        }
+
+        input.close();
     }
 }
