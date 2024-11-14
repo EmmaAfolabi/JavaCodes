60ebe567a967d131e622a7f9dffea90e17f80ace
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-14 09:53:02 +0100

    Divisible.java

---
+++ src/Chapter6/Divisible.java
@@ -0,0 +1,12 @@
+package Chapter6;
+
+public class Divisible {
+    // method to check if a number is divisible by 5
+    public static boolean isDivisible(int number) {
+        return number % 5 == 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
