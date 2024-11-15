54b8925cd8efbc7adfa87d8696d69b6aadb9184e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-15 10:47:35 +0100

    PrimeNumbers

--- src/Chapter6/PrimeNumbers.java
+++ src/Chapter6/PrimeNumbers.java
@@ -1,2 +1,17 @@
-package Chapter6;public class PrimeNumbers {
+package Chapter6;
+
+public class PrimeNumbers {
+
+    public static boolean findPrime(int number) {
+
+        if (number <= 1) return false;
+        if (number == 2 || number == 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+
+        int sqrt = (int) Math.sqrt(number);
+        for (int i = 5; i <= sqrt; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) return false;
+        }
+        return true;
+    }
 }
