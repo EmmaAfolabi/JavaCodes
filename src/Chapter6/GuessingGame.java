ee46b15f5e1b0ecfa5c4b392483f4bf7b2b985ec
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-17 11:07:02 +0100

    GuessingGame

--- src/Chapter6/GuessingGame.java
+++ src/Chapter6/GuessingGame.java
@@ -13,5 +13,42 @@ public class GuessingGame {
         int guess, play = 1, tries = 0;
         int value;
         Mode range = Mode.Default;
+
+        while (play == 1) {
+            System.out.println("Guess a number between 1 -" + MAX_NUMBER);
+            value = generateNumbers(MAX_NUMBER);
+
+            do {
+                guess = input.nextInt();
+
+                if (guess > value) {
+                    range = Mode.High;
+                    System.out.println("Too High. Try Again");
+                } else if (guess < value) {
+                    range = Mode.Low;
+                    System.out.println("Too Low. Try Again");
+                } else {
+                    range = Mode.Yes;
+                    System.out.printf("Congratulations. You guessed the number!!%n%n");
+                    break;
+                }
+                tries++;
+            } while (range != Mode.Yes);
+
+            if (tries <= 10) {
+                System.out.println("Either you know the secret or you got lucky!\n");
+            } else if (tries == 10) {
+                System.out.println("Aha! you know the secret! \n");
+            } else {
+                System.out.println("You should eb able to do better\n");
+            }
+
+            tries = 0;
+            range = Mode.Default;
+            System.out.print("Enter 1 to play again or 0 to exit\n");
+            play = input.nextInt();
+        }
+        System.out.print("Goodbye!!");
+        input.close();
     }
 }
