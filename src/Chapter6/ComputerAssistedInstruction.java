f500609df50e7642ef90ec2f2dc4ecb9b62d8745
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-20 10:50:44 +0100

    ComputerAssistedInstruction

--- src/Chapter6/ComputerAssistedInstruction.java
+++ src/Chapter6/ComputerAssistedInstruction.java
@@ -14,5 +14,29 @@ public class ComputerAssistedInstruction {
         String prompt = """
                 Welcome to the application of multiplication
                 """;
+        System.out.println(prompt);
+        newQuestions();
+        userMyInput = input.nextInt();
+        int totalRandomValue = randomValue * randomValue2;
+        answerToQuestion(userMyInput, totalRandomValue);
+    }
+
+    public static void newQuestions() {
+        randomValue = 1 + randomNumbers.nextInt(9);
+        randomValue2 = 1 + randomNumbers.nextInt(9);
+        System.out.println("How much is " + randomValue + " times " + randomValue2 + " ?");
+        userMyInput = input.nextInt();
+        answerToQuestion(userMyInput, randomValue * randomValue2);
+    }
+
+    public static void answerToQuestion(int userInput, int totalRandomValues) {
+        if (userInput == totalRandomValues) {
+            System.out.println("Good work!");
+            newQuestions();
+        } else {
+            System.out.println("Try again");
+            userInput = input.nextInt();
+            answerToQuestion(userInput, randomValue * randomValue2);
+        }
     }
 }
