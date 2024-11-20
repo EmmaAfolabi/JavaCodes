8b6d64f0e78c5925dae92fbf3074440d481bad97
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-20 10:27:56 +0100

    ComputerAssistedInstruction

--- src/Chapter6/ComputerAssistedInstruction.java
+++ src/Chapter6/ComputerAssistedInstruction.java
@@ -4,33 +4,15 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class ComputerAssistedInstruction {
+    private static Scanner input = new Scanner(System.in);
+    private static SecureRandom randomNumbers = new SecureRandom();
+    static int randomValue;
+    static int randomValue2;
+    static int userMyInput;
 
-    static SecureRandom numbers = new SecureRandom();
-    static Scanner input = new Scanner(System.in);
-
-    private static int level = 1;
-
-    public static int generateMultiplication() {
-        int digit1 = 0;
-        int digit2 = 0;
-
-        if (level == 1) {
-            digit1 = 1 + numbers.nextInt(9);
-            digit2 = 1 + numbers.nextInt(9);
-        }
-        else if (level == 2) {
-            digit1 = 1 + numbers.nextInt(90);
-            digit2 = 1 + numbers.nextInt(90);
-        }
-        else if (level == 3) {
-            digit1 = 1 + numbers.nextInt(900);
-            digit2 = 1 + numbers.nextInt(900);
-        }
-
-        System.out.printf("How much is %d times %d ?%n", digit1, digit2);
-        int product = digit1 * digit2;
-
-        return product;
-
+    public static void main(String[] args) {
+        String prompt = """
+                Welcome to the application of multiplication
+                """;
     }
 }
