30ba83bd8ddc091112f567d1d32d55f6e958cd3c
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-24 20:04:27 +0100

    ComputerAssistedInstruction

--- src/Chapter6/ComputerAssistedInstruction.java
+++ src/Chapter6/ComputerAssistedInstruction.java
@@ -33,6 +33,75 @@ public class ComputerAssistedInstruction {
         return product;
     }
 
+    public static int generateAddition() {
+        int digit1 = 0;
+        int digit2 = 0;
+
+        if (level == 1) {
+            digit1 = 1 + numbers.nextInt(9);
+            digit2 = 1 + numbers.nextInt(9);
+        } else if (level == 2) {
+            digit1 = 1 + numbers.nextInt(90);
+            digit2 = 1 + numbers.nextInt(90);
+        } else if (level == 3) {
+            digit1 = 1 + numbers.nextInt(900);
+            digit2 = 1 + numbers.nextInt(900);
+        }
+
+        System.out.printf("How much is %d plus %d ?%n", digit1, digit2);
+        int sum = digit1 + digit2;
+        return sum;
+    }
+
+    public static int generateSubtraction() {
+        int digit1 = 0;
+        int digit2 = 0;
+
+        if (level == 1) {
+            digit1 = 1 + numbers.nextInt(9);
+            digit2 = 1 + numbers.nextInt(9);
+        } else if (level == 2) {
+            digit1 = 1 + numbers.nextInt(90);
+            digit2 = 1 + numbers.nextInt(90);
+        } else if (level == 3) {
+            digit1 = 1 + numbers.nextInt(900);
+            digit2 = 1 + numbers.nextInt(900);
+        }
+
+        System.out.printf("How much is %d minus %d ?%n", digit1, digit2);
+        int sub = digit1 - digit2;
+        return sub;
+    }
+
+    public static double generateDivision() {
+        int digit1 = 0;
+        int digit2 = 0;
+        double quotient = 0;
+        if (level == 1) {
+            digit1 = 2 + numbers.nextInt(10);
+            digit2 = 2 + numbers.nextInt(10);
+        }
+        else if (level == 2) {
+            digit1 = 1 + numbers.nextInt(90);
+            digit2 = 1 + numbers.nextInt(90);
+        }
+        else if (level == 3) {
+            digit1 = 1 + numbers.nextInt(900);
+            digit2 = 1 + numbers.nextInt(900);
+        }
+
+        if (digit1 > digit2) {
+            System.out.printf("What is the quotient of %d divided by %d ?%n", digit1, digit2);
+            quotient = (double) digit1 / digit2;
+        }
+        else if (digit2 > digit1) {
+            System.out.printf("What is the quotient of %d divided by %d ?%n", digit2, digit1);
+            quotient = (double) digit2 / digit1;
+        }
+
+        return quotient;
+    }
+
     public static void main(String[] args) {
         while (true) {
             int correctAnswer = generateMultiplication();
