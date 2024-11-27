d8cd3efa07c2a482dbf311234309c5771528ba79
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-28 00:38:24 +0100

    InitArray

--- src/Chapter7/InitArray.java
+++ src/Chapter7/InitArray.java
@@ -3,7 +3,13 @@ package Chapter7;
 public class InitArray {
     public static void main(String[] args) {
         // declare variable array and initialize it with an array object
-        int[] array = new int[]; // create the array object
+        int[] array = new int[10];//create the array object
 
+        System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+
+        // output each array element's value
+        for (int counter = 0; counter < array.length; counter++) {
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
     }
 }
