2fd9aa8d601da433ee061a84953b33f7e2e75b8b
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-28 00:20:49 +0100

    InitArray

--- src/Chapter6/GuessingGame.java
+++ src/Chapter6/GuessingGame.java
@@ -13,7 +13,7 @@ public class GuessingGame {
     public static void guessingGame() {
         final int MAX_NUMBER = 1000;
         final int MAX_TRIES = 10;  // set the maximum number of trials to 10
-        final int TIME_LIMIT = 60; // Time limit in seconds
+        final int TOTAL_TIME_LIMIT = 60; // Time limit in seconds
         Scanner input = new Scanner(System.in);
 
         int guess, play = 1, tries = 0;
@@ -24,39 +24,63 @@ public class GuessingGame {
             System.out.printf("Guess a number between 1 - %d:", MAX_NUMBER);
             value = generateNumbers(MAX_NUMBER);
 
-            // Start the timer
-            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+            // Start the overall timer
+            ScheduledExecutorService overallExecutor = Executors.newScheduledThreadPool(1);
             final boolean[] timeUp = {false};
-            executorService.schedule(() -> {
+            overallExecutor.schedule(() -> {
                 timeUp[0] = true;
                 System.out.println("\nTime's up");
-            }, TIME_LIMIT, TimeUnit.SECONDS);
+            }, TOTAL_TIME_LIMIT, TimeUnit.SECONDS);
 
-            do {
-                guess = input.nextInt();
-                tries++;
+            while (!timeUp[0] && range != Mode.Yes) {
+                ScheduledExecutorService trialExecutor = Executors.newScheduledThreadPool(1);
+                final boolean[] trialTimeUp = {false};
+                final int TRIAL_TIME_LIMIT = 10; // Time limit for each trial in seconds
 
-                if (guess > value) {
-                    range = Mode.High;
-                    System.out.println("Too High. Try Again");
-                } else if (guess < value) {
-                    range = Mode.Low;
-                    System.out.println("Too Low. Try Again");
-                } else {
-                    range = Mode.Yes;
-                    System.out.printf("Congratulations. You guessed the number!!%n%n");
+                trialExecutor.schedule(() -> {
+                    trialTimeUp[0] = true;
+                    System.out.println("Trial time's up! Move faster next time!");
+                }, TRIAL_TIME_LIMIT, TimeUnit.SECONDS);
+
+                if (timeUp[0]) {
+                    System.out.printf("You've exceeded the total time limit of %d seconds.%n", TOTAL_TIME_LIMIT);
                     break;
                 }
 
-                if (tries > MAX_TRIES) {
-                    System.out.println("Sorry, you've exceeded the maximum number of trials. The game will end now.");
-                    System.out.printf("The correct number was %d.%n%n", value);
-                    break;
+                if (input.hasNext()) {
+                    guess = input.nextInt();
+                    tries++;
+
+                    if (trialTimeUp[0]) {
+                        System.out.println("Your trial took too long! Moving to the next trial.");
+                        continue;
+                    }
+
+                    if (guess > value) {
+                        range = Mode.High;
+                        System.out.println("Too High. Try Again");
+                    } else if (guess < value) {
+                        range = Mode.Low;
+                        System.out.println("Too Low. Try Again");
+                    } else {
+                        range = Mode.Yes;
+                        System.out.printf("Congratulations. You guessed the number!!%n%n");
+                        break;
+                    }
+
+                    if (tries > MAX_TRIES) {
+                        System.out.println("Sorry, you've exceeded the maximum number of trials. The game will end now.");
+                        System.out.printf("The correct number was %d.%n%n", value);
+                        break;
+                    }
+
                 }
+                trialExecutor.shutdown();
+            }
+
+            overallExecutor.shutdown();
                 //tries++;
-            } while (range != Mode.Yes);
-
-            executorService.shutdown();
+            //} while (range != Mode.Yes);
 
             if (range == Mode.Yes) {
                 System.out.printf("You guessed the correct number in %d tries!%n%n", tries);
@@ -79,7 +103,13 @@ public class GuessingGame {
             tries = 0;
             range = Mode.Default;
             System.out.print("Enter 1 to play again or 0 to exit\n");
+
+            // reset and display the timer before making a choice
+            long startTime = System.currentTimeMillis();
             play = input.nextInt();
+            long endTime = System.currentTimeMillis();
+            long duration = (endTime - startTime) / 1000;
+            System.out.printf("Time taken to decide: %d seconds%n", duration);
         }
         System.out.print("Goodbye!!");
         input.close();
---
+++ src/Chapter7/InitArray.java
@@ -0,0 +1,9 @@
+package Chapter7;
+
+public class InitArray {
+    public static void main(String[] args) {
+        // declare variable array and initialize it with an array object
+        int[] array = new int[]; // create the array object
+
+    }
+}
