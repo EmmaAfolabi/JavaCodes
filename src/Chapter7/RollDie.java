a1a6f026d964c6048bf92b1eff832f441decf497
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-29 14:57:45 +0100

    RollDie

---
+++ src/Chapter7/RollDie.java
@@ -0,0 +1,13 @@
+package Chapter7;
+
+import java.security.SecureRandom;
+
+public class RollDie {
+    public static void main(String[] args) {
+        SecureRandom randomNumbers = new SecureRandom();
+        int [] frequency = new int[7];  // array of frequency counters
+
+        // roll die 60,000,000 times; use die value as frequency index
+    }
+
+}
