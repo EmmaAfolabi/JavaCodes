863c53c5d1bdb701f4d4fc7bc333c52dbae2d5a8
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-11-29 01:03:29 +0100

    BarChart

---
+++ src/Chapter7/BarChart.java
@@ -0,0 +1,10 @@
+package Chapter7;
+
+
+public class BarChart {
+    public static void main(String[] args) {
+        int [] array = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1};
+
+        System.out.println("Grade distribution");
+    }
+}
