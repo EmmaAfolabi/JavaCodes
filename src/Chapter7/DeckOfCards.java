f4f3d27a6dd9f077d737245bb0d172d637d0b077
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-01 22:22:50 +0100

    DeckOfCards

---
+++ src/Chapter7/DeckOfCards.java
@@ -0,0 +1,22 @@
+package Chapter7;
+
+import java.security.SecureRandom;
+
+public class DeckOfCards {
+    // random number generator
+    private static final SecureRandom randomNumbers = new SecureRandom();
+    private static final int NUMBER_OF_CARDS = 52;  // constant # of cards
+
+    private Card[] deck = new Card[NUMBER_OF_CARDS];  // card references
+    private int currentCard = 0;  // index of next Card to be dealt (0-51)
+
+    // constructor fills deck of Cards
+    public DeckOfCards() {
+        String[] faces = {"Ace", "Deuce", "Three", "Four", "Five", "Six",
+          "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        // populate deck with Card objects
+
+    }
+}
