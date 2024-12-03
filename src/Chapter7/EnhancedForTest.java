da19dc4d30135db4db241aa9228aca622c49cbc1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-04 00:18:06 +0100

    EnhancedForTest

---
+++ src/Chapter7/EnhancedForTest.java
@@ -0,0 +1,12 @@
+package Chapter7;
+
+
+public class EnhancedForTest {
+    public static void main(String[] args) {
+        int [] array = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+        int total = 0;
+
+        // add each element's value to total
+
+    }
+}
