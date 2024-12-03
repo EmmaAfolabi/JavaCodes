6777465beb14bbc90c8887eb37f6439b2036c974
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-04 00:42:51 +0100

    PassArray

---
+++ src/Chapter7/PassArray.java
@@ -0,0 +1,13 @@
+package Chapter7;
+
+
+public class PassArray {
+    // main creates array and calls modifyArray and modifyElement
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.printf(
+                "Effects of passing reference to entire array:%n" +
+                        "The values of the original array are:%n");
+    }
+}
