92f5fdea81ae4686d91c020d8152fe2c3b2f9527
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-04 00:33:29 +0100

    EnhancedForTest

--- src/Chapter7/EnhancedForTest.java
+++ src/Chapter7/EnhancedForTest.java
@@ -7,6 +7,10 @@ public class EnhancedForTest {
         int total = 0;
 
         // add each element's value to total
+        for (int number : array) {
+            total += number;
+        }
 
+        System.out.printf("Total of array elements: %d%n", total);
     }
 }
