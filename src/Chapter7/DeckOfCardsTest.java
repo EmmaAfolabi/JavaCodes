dca09a4d83cc952acaed93478aed3e286fd0a2a6
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-03 23:29:24 +0100

    Card

--- src/Chapter7/DeckOfCardsTest.java
+++ src/Chapter7/DeckOfCardsTest.java
@@ -10,7 +10,11 @@ public class DeckOfCardsTest {
         // print all 52 cards in the order in which they are dealt
         for (int i = 1; i <= 52; i++) {
             // deal and display a Card
-
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
+            
+            if (i % 4 == 0) {  // output a newline after every fourth card
+                System.out.println();
+            }
         }
     }
 }
