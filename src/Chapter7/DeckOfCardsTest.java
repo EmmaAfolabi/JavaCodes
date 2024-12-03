e87186aa20f81b19b4b317967be49a7982052ee5
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-03 12:48:54 +0100

    DeckOfCardsTest

---
+++ src/Chapter7/DeckOfCardsTest.java
@@ -0,0 +1,16 @@
+package Chapter7;
+
+
+public class DeckOfCardsTest {
+    // execute application
+    public static void main(String[] args) {
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle();  // place cards in random order
+
+        // print all 52 cards in the order in which they are dealt
+        for (int i = 1; i <= 52; i++) {
+            // deal and display a Card
+
+        }
+    }
+}
