83a7840f5cfdbde03b6470bd3067373d109b995b
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-09 00:12:26 +0100

    PassArray

--- src/Chapter7/PassArray.java
+++ src/Chapter7/PassArray.java
@@ -17,5 +17,27 @@ public class PassArray {
 
         modifyArray(array);  // pass array reference
         System.out.printf("%n%nThe values of the modified array are:%n");
+
+        // output modified array elements
+        for (int value : array) {
+            System.out.printf("   %d", value);
+        }
+
+        System.out.printf(
+                "%n%nEffects of passing array element value:%n" +
+                        "array[3] before modifyElement: %d%n", array[3]);
+
+        modifyElement(array[3]);  // attempt to modify array[3]
+        System.out.printf(
+                "array[3] after modifyElement: %d%n", array[3]);
     }
+
+    // multiply each element of an array by 2
+    public static void modifyArray(int[] array2) {
+        for (int counter = 0; counter < array2.length; counter++) {
+            array2[counter] *= 2;
+        }
+    }
+
+
 }
