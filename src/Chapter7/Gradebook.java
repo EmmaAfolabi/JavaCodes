683de4fb6609bd7895b49291f4ac70b6027d7abc
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-13 23:35:43 +0100

    Gradebook

--- src/Chapter7/Gradebook.java
+++ src/Chapter7/Gradebook.java
@@ -4,4 +4,41 @@ package Chapter7;
 public class Gradebook {
     private String courseName;  // name of course this Gradebook represents
     private int[] grades;  // array of student grades
+
+    // constructor
+    public Gradebook(String courseName, int[] grades) {
+        this.courseName = courseName;
+        this.grades = grades;
+    }
+
+    // method to set the course name
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    // method to retrieve the course name
+    public String getCourseName() {
+        return courseName;
+    }
+
+    // perform various operations on the data
+    public void processGrades() {
+        // output grades array
+        outputGrades();
+
+        // call method getAverage to calculate the average grade
+        System.out.printf("%nClass average is %.2f%n", getAverage());
+
+        // call methods getMinimum and getMaximum
+        System.out.printf("Lowest grade is %d%nHighest grade is %d%n%n",
+                getMinimum(), getMaximum());
+
+        // call outputBarChart to print grade distribution chart
+        outputBarChart();
+    }
+
+    // find minimum grade
+    public int getMinimum() {
+        int lowGrade = grades[0];  // assume grades[0] is smallest
+    }
 }
