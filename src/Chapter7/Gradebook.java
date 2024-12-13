ba0e8fac4e7ad74d038b226c0bd4485ab328f8d7
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-13 23:08:24 +0100

    Gradebook

---
+++ src/Chapter7/Gradebook.java
@@ -0,0 +1,7 @@
+package Chapter7;
+// Gradebook class using an array to store test grades
+
+public class Gradebook {
+    private String courseName;  // name of course this Gradebook represents
+    private int[] grades;  // array of student grades
+}
