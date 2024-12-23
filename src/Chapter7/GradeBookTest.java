81178692ab6e14594583649089f31113e08658ae
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-23 07:54:20 +0100

    GradeBookTest

--- src/Chapter7/GradeBookTest.java
+++ src/Chapter7/GradeBookTest.java
@@ -3,6 +3,11 @@ package Chapter7;
 // then invokes method processGrades to analyze them
 public class GradeBookTest {
     public static void main(String[] args) {
+        // array of student grades
+        int[] gradesArray = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
 
+        Gradebook myGradeBook = new Gradebook(
+                "CS101 Introduction to Java Programming", gradesArray);
+        System.out.printf("");
     }
 }
