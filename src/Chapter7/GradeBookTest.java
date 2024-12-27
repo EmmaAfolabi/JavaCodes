5b48099599e05d8f39bd63e6fa83af8f51c373a5
emmanuel Afolabi <daveemmanuel985@gmail.com>
2024-12-27 23:47:05 +0100

    GradeBookTest

--- src/Chapter7/GradeBookTest.java
+++ src/Chapter7/GradeBookTest.java
@@ -10,5 +10,6 @@ public class GradeBookTest {
                 "CS101 Introduction to Java Programming", gradesArray);
         System.out.printf("Welcome to the grade book for%n%s%n%n",
                 myGradeBook.getCourseName());
+        myGradeBook.processGrades();
     }
 }
