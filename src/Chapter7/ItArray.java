718a32ff7a828822dd963bf8750c50920f412be1
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-01-20 12:03:44 +0100

    ItArray

---
+++ src/Chapter7/ItArray.java
@@ -0,0 +1,9 @@
+package Chapter7;
+
+public class ItArray {
+    // create and output two-dimensional arrays
+    public static void main(String[] args) {
+        int [] [] array1 = {{1, 2, 3}, {4, 5, 6}};
+        int [] [] array2 = {{1, 2}, {3}, {4, 5, 6}};
+    }
+ }
