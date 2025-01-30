bf958d25ef90734e9edd20a118ca639bf71a8da7
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-01-30 14:35:28 +0100

    ItArray

--- src/Chapter7/ItArray.java
+++ src/Chapter7/ItArray.java
@@ -1,9 +1,16 @@
 package Chapter7;
 
+
 public class ItArray {
     // create and output two-dimensional arrays
     public static void main(String[] args) {
         int [] [] array1 = {{1, 2, 3}, {4, 5, 6}};
         int [] [] array2 = {{1, 2}, {3}, {4, 5, 6}};
+
+        System.out.println("Values in array1 by row are");
+        outputArray(array1);  // displays array1 by row
+
+        System.out.printf("%nValues in array2 by row are%n");
+        outputArray(array2); // displays array2 by row
     }
  }
