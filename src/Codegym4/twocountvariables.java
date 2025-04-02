776a58c7a37029e6bbe97a68ba32699e458ef81c
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-04-02 23:55:25 +0100

    twocountvariables

---
+++ src/Codegym4/twocountvariables.java
@@ -0,0 +1,12 @@
+package Codegym4;
+
+public class twocountvariables {
+    public int count = 0;
+
+    public void run()
+    {
+        count = 15;
+        int count = 10;
+        count++;
+    }
+}
