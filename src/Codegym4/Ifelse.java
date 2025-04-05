109a8e6646ab175b9e5c51b699cd51d55f4e494c
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-04-06 00:21:18 +0100

    Ifelse

--- src/Codegym4/Ifelse.java
+++ src/Codegym4/Ifelse.java
@@ -2,7 +2,7 @@ package Codegym4;
 
 public class Ifelse {
    public static void main(String[] args) {
-       int hour = 18;
+       int hour = 10;
        String greeting = "Hello, good ";
        if (hour >= 0 && hour <= 12) {
            System.out.println(greeting + "morning!");
