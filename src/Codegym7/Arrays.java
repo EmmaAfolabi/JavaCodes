a95f42af7e7cbf6d11fdb92a3d134a7122bb6802
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-04-06 13:11:56 +0100

    Arrays

---
+++ src/Codegym7/Arrays.java
@@ -0,0 +1,14 @@
+package Codegym7;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //bytes
+
+        int[] a;
+        double[] numbers = new double[5];
+        a = new int[5];
+        a[3] = 5; // 0,1,2,3,4
+        System.out.println(a[3]);
+        System.out.println(a[2]);
+    }
+}
