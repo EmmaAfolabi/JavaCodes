b6270bb4395d8224d72dd1ccfdaf822510782d5d
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-04-10 12:39:09 +0100

    Readnumbers

--- src/Codegym7/Generics/Readnumbers.java
+++ src/Codegym7/Generics/Readnumbers.java
@@ -3,20 +3,26 @@ package Codegym7.Generics;
 import Codegym7.ArrayList;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Readnumbers {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    ArrayList<Integer> list = new ArrayList<Integer>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
-    while (true) {
-        String s = reader.readLine();
-        if (s.isEmpty()) break;
+        list.add(1);
+        list.add(7);
+        list.add(11);
+        list.add(3);
+        list.add(15);
 
-        int x = Integer.parseInt(s);
-        if (x % 2 == 0)  // check that the remainder is zero when we divide by two
-            list.add(x);    // add to the end
-        else
-            list.add(0, x);  // add to the beginning
+        for (int i = 0; i < list.size; i++) {
+            if (list.get(i) > 5)
+                list.remove(i);
+            else
+                i++;
+        }
     }
 }
+
