fa100a41a14625406bc6c1eac5101d681ff3e586
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-04-11 10:32:49 +0100

    Mergelists

---
+++ src/Codegym7/Generics/Mergelists.java
@@ -0,0 +1,25 @@
+package Codegym7.Generics;
+
+import Codegym7.ArrayList;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class Mergelists {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();    // Create a list
+        Collections.addAll(list1,1, 5, 6, 11, 3, 15, 7, 8);    // Fill the list
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        Collections.addAll(list2, 1, 8, 6, 21, 53, 5, 67, 18);
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        result.addAll(list1);  // Add all values from each list to the new list
+        result.addAll(list2);
+
+        for (Integer x : result) {  // A fast way to loop over all elements, only for collections
+            System.out.println(x);
+        }
+    }
+}
