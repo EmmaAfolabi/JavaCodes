59cd6852871073b3e124400c2ad7c7fd4c75e5e7
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-15 12:28:40 +0100

    LargeNumbersthan5

---
+++ src/Codegym7/Generics/LargeNumbersthan5.java
@@ -0,0 +1,28 @@
+package Codegym7.Generics;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class LargeNumbersthan5 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(7);
+        list.add(11);
+        list.add(3);
+        list.add(15);
+
+        for (int i = 0; i < list.size(); )
+        {
+            if (list.get(i) > 5)
+                list.remove(i);
+            else
+                i++;
+        }
+
+        System.out.println("Numbers <= 5: " + list);
+    }
+}
