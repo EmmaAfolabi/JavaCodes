7290db0f29f48a517a4e5589b1ff4cb20b806829
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-16 10:32:55 +0100

    DivideArray

---
+++ src/Codegym7/Generics/DivideArray.java
@@ -0,0 +1,25 @@
+package Codegym7.Generics;
+
+import java.util.ArrayList;
+
+public class DivideArray {
+    public static void main(String[] args) {
+        int[] data = {1, 5, 6, 11, 3, 15, 7, 8};
+
+        ArrayList<Integer> list = new ArrayList<Integer> ();
+
+        for (int i = 0; i < data.length; i++) list.add(data[i]);
+
+        ArrayList<Integer> even = new ArrayList<Integer>(); // Even number
+        ArrayList<Integer> odd = new ArrayList<Integer>(); // Odd number
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            Integer x = list.get(i);
+            if (x % 2 == 0)
+                even.add(x);
+            else
+                odd.add(x);
+        }
+    }
+}
