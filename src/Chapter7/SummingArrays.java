97a69ab2eef6473ba9640509f3725c2c0a403a8c
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-22 13:15:35 +0100

    SummingArrays

---
+++ src/Chapter7/SummingArrays.java
@@ -0,0 +1,8 @@
+package Chapter7;
+
+public class SummingArrays {
+    public static void main(String[] args) {
+        int[] array = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+        int total = 0;
+    }
+}
