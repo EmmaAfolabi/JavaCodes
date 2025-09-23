af030fcff8ffb44e5ea0a4169dd96e21bee92193
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-23 20:10:33 +0100

    Gradebook2D

--- src/Chapter7/Gradebook2D.java
+++ src/Chapter7/Gradebook2D.java
@@ -100,5 +100,50 @@ public class Gradebook2D {
             }
         }
 
+        // for each grade frequency, print bar in chart
+        for (int count = 0; count < frequency.length; count++) {
+            // output bar label ("00-09: ", ..., "90-99: ", "100: ")
+            if (count == 10) {
+                System.out.printf("%5d: ", 100);
+            }
+            else {
+                System.out.printf("%02d-%02d: ",
+                        count * 10, count * 10 + 9);
+            }
+
+            // print bar of asterisks
+            for (int stars = 0; stars < frequency[count]; stars++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    // output the contents of the grades array
+    public void outputGrades() {
+        System.out.printf("The grades are:%n%n");
+        System.out.println("          "); // align column heads
+
+        // create a column heading for each of the tests
+        for (int test = 0; test < grades[0].length; test++) {
+            System.out.printf("Test %d  ", test + 1);
+        }
+
+        System.out.println("Average"); // student average column heading
+
+        // create rows/columns of text representing array grades
+        for (int student = 0; student < grades.length; student++) {
+            System.out.printf("Student %2d", student + 1);
+
+            for (int test : grades[student]) { // output student grades
+                System.out.printf("%8d", test);
+            }
+
+            // call method getAverage to calculate student's average grade
+            // pass row of grades as the argument to getAverage
+            double average = getAverage(grades[student]);
+            System.out.printf("%9.2f%n", average);
+        }
     }
 }
