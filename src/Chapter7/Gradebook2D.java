45e049a7ebab45a4adae4e796e1f92eb4e03e18c
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-23 19:01:46 +0100

    Gradebook2D

--- src/Chapter7/Gradebook2D.java
+++ src/Chapter7/Gradebook2D.java
@@ -53,4 +53,26 @@ public class Gradebook2D {
 
         return lowGrade;
     }
+
+    // find maximum grade
+    public int getMaximum() {
+        // assume first element of grades array is largest
+        int highGrade = grades[0][0];
+
+        // loop through rows of grades array
+        for (int[] studentGrades : grades) {
+            // loop through columns of current row
+            for (int grade : studentGrades) {
+                // if grade greater than highGrade, assign it to highGrade
+                if (grade > highGrade) {
+                    highGrade = grade;
+                }
+            }
+        }
+
+        return highGrade;
+    }
+
+    // determine average grade for particular set of grades
+
 }
