81f937cf367f2addf8461c84ae9569d2495a23c8
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-24 13:10:45 +0100

    Gradebook2DTest

--- src/Chapter7/Gradebook2DTest.java
+++ src/Chapter7/Gradebook2DTest.java
@@ -2,8 +2,20 @@ package Chapter7;
 // Grade bookTest creates Grade book object using a two-dimensional array
 // of grades, then invokes method processGrades to analyze them.
 public class Gradebook2DTest {
+    // main method begins program execution
     public static void main(String[] args) {
-        // main method begins program execution
+        // two-dimensional array of student grades
+        int[][] gradesArray = { {87, 96, 70},
+                                {68, 87, 90},
+                                {94, 100, 90},
+                                {100, 81, 82},
+                                {83, 65, 85},
+                                {78, 87, 65},
+                                {85, 75, 83},
+                                {91, 94, 100},
+                                {76, 72, 84},
+                                {87, 93, 73}};
+
     }
 
 }
