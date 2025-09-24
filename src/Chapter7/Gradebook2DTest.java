b84f58efdc1d844fdbed5765c1482056ae91bf82
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-24 13:17:13 +0100

    Gradebook2DTest

--- src/Chapter7/Gradebook2DTest.java
+++ src/Chapter7/Gradebook2DTest.java
@@ -20,6 +20,6 @@ public class Gradebook2DTest {
                 "CSC 101 Introduction to Java Programming", gradesArray);
         System.out.printf("Welcome to the grade book for%n%s%n%n",
                 myGradebook.getCourseName());
+        myGradebook.processGrades();
     }
-
 }
