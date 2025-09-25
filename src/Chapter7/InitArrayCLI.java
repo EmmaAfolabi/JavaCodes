c2f9c9ad8c947825e7ed0cd3894bbbacd800907e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-25 14:25:01 +0100

    InitArrayCLI

--- src/Chapter7/InitArrayCLI.java
+++ src/Chapter7/InitArrayCLI.java
@@ -24,6 +24,11 @@ public class InitArrayCLI {
             }
 
             System.out.printf("%s%8s%n", "Index", "Value");
+
+            // display array index and value
+            for (int counter = 0; counter < array.length; counter++) {
+                System.out.printf("%5d%8d%n", counter, array[counter]);
+            }
         }
     }
 }
