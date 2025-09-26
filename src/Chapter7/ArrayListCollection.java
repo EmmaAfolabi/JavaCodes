da0311f46e688b0b64bf9a0b75fc16d9612277f2
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-26 13:47:26 +0100

    ArrayListCollection

--- src/Chapter7/ArrayListCollection.java
+++ src/Chapter7/ArrayListCollection.java
@@ -18,5 +18,38 @@ public class ArrayListCollection {
         for (int i = 0; i < items.size(); i++) {
             System.out.printf(" %s", items.get(i));
         }
+
+        // display colors using enhanced for int the display method
+        display(items,
+                "%nDisplay list contents with enhanced for statement:");
+
+        items.add("green"); // add "green" to the end of the list
+        items.add("yellow"); // add "green" to the end of the list
+        display(items, "List with two new elements:");
+
+        items.remove("yellow"); // remove the first "yellow"
+        display(items, "Remove first instance of yellow:");
+
+        items.remove(1); // remove item at index 1
+        display(items, "Remove second list element (green):");
+
+        // check if a value is in the list
+        System.out.printf("\"red\" is %sin the list%n",
+                items.contains("red") ? "": "not");
+
+        // display number of elements in the list
+        System.out.printf("Size: %s%n", items.size());
+    }
+
+    // display the ArrayList's elements on the console
+    public static void display(ArrayList<String> items, String header) {
+        System.out.printf("header"); // display header
+
+        // display each element in items
+        for (String item : items) {
+            System.out.printf(" %s", item);
+        }
+
+        System.out.println();
     }
 }
