e0a08261a8d3bc166cf6f353188d519f651484df
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-29 15:41:15 +0100

    DuplicateElimination

--- src/Chapter7/DuplicateElimination.java
+++ src/Chapter7/DuplicateElimination.java
@@ -8,5 +8,17 @@ public class DuplicateElimination {
         Scanner scanner = new Scanner(System.in);
         int[] nums = new int[10];
         Arrays.fill(nums, -1);
+
+        // Input: exactly 10 numbers
+        for (int i = 0; i < nums.length; i++) {
+            int v;
+            while (true) {
+                System.out.println("Enter number " + (i + 1) + " (10-100): ");
+                v = scanner.nextInt();
+                if (v >= 10 && v <= 100) break;
+                System.out.println("Out of range. Try again.");
+            }
+            nums[i] = v;
+        }
     }
 }
