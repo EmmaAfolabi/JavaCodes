02e3b0216d4f3e453d4c4bd28b65589f66863cdb
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-09-29 15:26:12 +0100

    DuplicateElimination

---
+++ src/Chapter7/DuplicateElimination.java
@@ -0,0 +1,12 @@
+package Chapter7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DuplicateElimination {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = new int[10];
+        Arrays.fill(nums, -1);
+    }
+}
