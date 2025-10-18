feb72edeffe84a4d3c66abd4c7794cb451f69469
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-18 01:33:40 +0100

    TortoiseandHareSimulationTest

---
+++ src/Chapter7/exercise7_28/TortoiseandHareSimulationTest.java
@@ -0,0 +1,13 @@
+package Chapter7.exercise7_28;
+
+public class TortoiseandHareSimulationTest {
+
+    public static void main(String[] args) {
+
+
+
+        TortoiseandHareSimulation mountain = new TortoiseandHareSimulation();
+
+        mountain.simulateRace();
+    }
+}
