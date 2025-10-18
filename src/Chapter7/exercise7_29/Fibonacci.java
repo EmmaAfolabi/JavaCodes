332f187e71337e43651218007479f93de1eaa187
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-18 01:41:06 +0100

    Fibonacci

---
+++ src/Chapter7/exercise7_29/Fibonacci.java
@@ -0,0 +1,25 @@
+package Chapter7.exercise7_29;
+
+public class Fibonacci {
+
+
+    public int findFibonacci(int nth) {
+
+        int [] fib = new int[nth];
+        int value;
+
+        fib[0] = 0; fib[1] = 1;
+
+
+        for (int term = 2; term < fib.length; term++) {
+
+            value = fib[term - 2] + fib[term - 1];
+            fib[term] = value;
+
+        }
+        for (int n: fib) {
+            System.out.println(n);
+        }
+        return fib[nth - 1];
+    }
+}
