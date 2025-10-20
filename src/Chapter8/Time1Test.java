2fcff8ca8d15f2ced4db18cf53de274ec4506688
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-21 00:52:14 +0100

    Time1Test

---
+++ src/Chapter8/Time1Test.java
@@ -0,0 +1,10 @@
+package Chapter8;
+// Time1 object used in an app
+
+public class Time1Test {
+    public static void main(String[] args) {
+        // create and initialize a Time1 object
+        Time1 time = new Time1(); // invokes Time1 constructor
+
+    }
+}
