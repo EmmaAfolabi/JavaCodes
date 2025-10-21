c5cc8b36398bc6fa61b1a5c4cdef1d6c9d9a5003
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-21 01:10:46 +0100

    Time1Test

--- src/Chapter8/Time1Test.java
+++ src/Chapter8/Time1Test.java
@@ -6,5 +6,10 @@ public class Time1Test {
         // create and initialize a Time1 object
         Time1 time = new Time1(); // invokes Time1 constructor
 
+        // output string representations of the time
+        displayTime("After time object is created", time);
+        System.out.println();
+
+
     }
 }
