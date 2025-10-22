78e2cabb7c762e84cbdb8da3739da1cdbe168fd5
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-22 18:21:50 +0100

    ThisTest

--- src/Chapter8/ThisTest.java
+++ src/Chapter8/ThisTest.java
@@ -18,6 +18,18 @@ class SimpleTime {
     // instance variable names the "this" reference is
     // required to distinguish between the names
     public SimpleTime(int hour, int minute, int second) {
-
+        this.hour = hour; // set "this"  object's hour
+        this.minute = minute; // set "this"  object's minute
+        this.second = second; // set "this" object's second
     }
+
+    // use explicit  and implicit "this" to call toUniversalString
+    public String buildString() {
+        return String.format("%24s: %s%n%24s: %s",
+                "this.toUniversalString()", this.toUniversalString(),
+                "toUniversalString()", toUniversalString());
+    }
+
+    // convert to String in universal-time format (HH:MM:SS)
+
 }
