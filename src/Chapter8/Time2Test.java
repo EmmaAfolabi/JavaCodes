f477b86967fe85bdb1316ea016833354fd0ebd94
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-27 11:54:51 +0100

    Time2Test

--- src/Chapter8/Time2Test.java
+++ src/Chapter8/Time2Test.java
@@ -8,5 +8,12 @@ public class Time2Test {
         Time2 t3 = new Time2(21, 34); // 21:34:00
         Time2 t4 = new Time2(12, 25, 42); // 12:25:42
         Time2 t5 = new Time2(t4); // 12:25:42
+
+        System.out.println("Constructed with:");
+        displayTime("t1: all default arguments", t1);
+        displayTime("t2: hour specified; default minute and second", t2);
+        displayTime("t3: hour and minute specified; default second", t3);
+        displayTime("t4: hour, minute and second specified", t4);
+        displayTime("t5: Time2 object t4 specified", t5);
     }
 }
