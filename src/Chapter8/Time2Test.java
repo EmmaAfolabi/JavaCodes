494b59671a183a42ff8f80588e5b38aa7a6b6825
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-27 11:33:52 +0100

    Time2Test

--- src/Chapter8/Time2Test.java
+++ src/Chapter8/Time2Test.java
@@ -3,6 +3,10 @@ package Chapter8;
 
 public class Time2Test {
     public static void main(String[] args) {
-
+        Time2 t1 = new Time2(); // 00:00:00
+        Time2 t2 = new Time2(2); // 02:00:00
+        Time2 t3 = new Time2(21, 34); // 21:34:00
+        Time2 t4 = new Time2(12, 25, 42); // 12:25:42
+        Time2 t5 = new Time2(t4); // 12:25:42
     }
 }
