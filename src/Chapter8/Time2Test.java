1276a9f6a29326cb2bb70432095e53903ad87213
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-28 23:33:35 +0100

    Time2Test

--- src/Chapter8/Time2Test.java
+++ src/Chapter8/Time2Test.java
@@ -15,5 +15,19 @@ public class Time2Test {
         displayTime("t3: hour and minute specified; default second", t3);
         displayTime("t4: hour, minute and second specified", t4);
         displayTime("t5: Time2 object t4 specified", t5);
+
+        // attempt to initialize t6 with invalid values
+        try {
+            Time2 t6 = new Time2(27, 74, 99); // invalid values
+        }
+        catch (IllegalArgumentException e) {
+            System.out.printf("%nException while initializing t6: %s%n",
+                    e.getMessage());
+        }
+    }
+
+    // displays a Time2 object in 24-hour and 12-hour formats
+    private static void displayTime(String header, Time2 t) {
+
     }
 }
