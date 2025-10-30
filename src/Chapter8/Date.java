ecd707b8f1c9cc00f4bebab2129580910ae87f62
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-31 00:45:15 +0100

    Date

---
+++ src/Chapter8/Date.java
@@ -0,0 +1,13 @@
+package Chapter8;
+// Date class declaration
+
+public class Date {
+    private int month; // 1-12
+    private int day; // 1-31 based on mouth
+    private int year; // any year
+
+    public static final int [] daysPerMonth =
+            {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+}
