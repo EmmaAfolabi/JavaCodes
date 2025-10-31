4f3a6032bea0003a0484a03df1938317ea7f7e65
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-31 15:19:49 +0100

    Date

--- src/Chapter8/Date.java
+++ src/Chapter8/Date.java
@@ -8,6 +8,16 @@ public class Date {
 
     public static final int [] daysPerMonth =
             {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    // constructor: confirm proper value for month and day given the year
+    public Date(int month, int day, int year) {
+        // check if month in range
+        if (month <= 0 || month > 12) {
+            throw new IllegalArgumentException(
+                    "month (" + month + ") must  be 1-12");
+        }
 
+        // check if day in range for month
 
+    }
 }
