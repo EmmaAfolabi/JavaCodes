3cb4612cebc63bf28fb6c970a0db27ee5b3b2ff7
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-31 16:27:29 +0100

    Date

--- src/Chapter8/Date.java
+++ src/Chapter8/Date.java
@@ -18,6 +18,28 @@ public class Date {
         }
 
         // check if day in range for month
+        if (day <= 0 ||
+                (day > daysPerMonth[month] && !(month == 2 && day == 29))) {
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+        }
 
+        // check for leap year if month is 2 and day is 29
+        if (month == 2 && day == 29 && !(year % 400 == 0 ||
+                (year % 4 == 0 && year % 100 != 0))) {
+            throw new IllegalArgumentException("day (" +  day +
+                    ") out-of-range for the specified month and year");
+        }
+
+        this.month = month;
+        this.day = day;
+        this.year = year;
+
+        System.out.printf("Date object constructor for date %s%n", this);
+    }
+
+    // return a String of the form month/day/year
+    public String toString() {
+        return String.format("%d/%d/%d", month, day, year);
     }
 }
