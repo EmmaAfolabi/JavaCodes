beb547f07a4ab701fc287982cb73625ef8205b41
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-10-31 17:09:47 +0100

    Employee

---
+++ src/Chapter8/Employee.java
@@ -0,0 +1,9 @@
+package Chapter8;
+// Employee class with references to other objects
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Date hireDate;
+}
