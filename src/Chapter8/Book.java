3710a323f8e9103248630ea8778c6067b70dbab2
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-01 23:40:10 +0100

    Book

--- src/Chapter8/Book.java
+++ src/Chapter8/Book.java
@@ -4,4 +4,11 @@ package Chapter8;
 
 public enum Book {
     // declare constants of enum type
+    JHTP("Java How to Program", "2018"),
+    CHTP("C How to Program", " 2016"),
+    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2017"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to program", "2017");
+
 }
