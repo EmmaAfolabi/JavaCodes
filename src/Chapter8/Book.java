39b961437bc3071f08dc1ae9156608f2674e9c90
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-01 23:33:04 +0100

    Book

---
+++ src/Chapter8/Book.java
@@ -0,0 +1,7 @@
+package Chapter8;
+// Declaring an enum type with a constructor and explicit instance fields
+// and accessors for these fields
+
+public enum Book {
+    // declare constants of enum type
+}
