8982536a35079b23bee26f9544efcb6c3bfa9f50
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-03 11:24:24 +0100

    EnumTest

--- src/Chapter8/EnumTest.java
+++ src/Chapter8/EnumTest.java
@@ -4,5 +4,11 @@ package Chapter8;
 public class EnumTest {
     public static void main(String[] args) {
         System.out.println("All books:");
+
+        // print all books in enum Book
+        for (Book book : Book.values()) {
+            System.out.printf("%-10s%-45s%s%n", book,
+                    book.getTitle(), book.getCopyrightYear());
+        }
     }
 }
