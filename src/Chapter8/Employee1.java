a2b97c4d8d61c60e8bc212c8b23c410af3ec53fe
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-03 16:10:48 +0100

    Employee1

--- src/Chapter8/Employee1.java
+++ src/Chapter8/Employee1.java
@@ -6,4 +6,15 @@ public class Employee1 {
     private static int count = 0; // number of Employees created
     private String firstName;
     private String lastName;
+
+    // initialize Employee, add 1 to static count and
+    // output String indicating that constructor was called
+    public Employee1(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        ++count; // increment static count of employees
+        System.out.printf("Employee constructor: %s %s; count = %d%n",
+                firstName, lastName, count);
+    }
 }
