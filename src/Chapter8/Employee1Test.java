c40b2bf1c87f048aa815d62a09830842aca9ad4e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-04 00:23:09 +0100

    Employee1Test

--- src/Chapter8/Employee1Test.java
+++ src/Chapter8/Employee1Test.java
@@ -6,5 +6,11 @@ public class Employee1Test {
         // show that count is 0 before creating Employees
         System.out.printf("Employees before instantiation: %d%n",
                 Employee1.getCount());
+
+        // create two Employees; count should be 2
+        Employee1 e1 = new Employee1("Susan", "Baker");
+        Employee1 e2 = new Employee1("Bob", "Blue");
+
+        // show that count is 2 after creating two employees
     }
 }
