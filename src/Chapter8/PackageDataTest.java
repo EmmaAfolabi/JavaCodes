ce37f427f1aa6f9d44d39a2af7dacac6620bf38f
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-04 12:15:40 +0100

    PackageDataTest

---
+++ src/Chapter8/PackageDataTest.java
@@ -0,0 +1,21 @@
+package Chapter8;
+// Package-access members of a class are accessible by other classes
+// in the same package
+
+public class PackageDataTest {
+    public static void main(String[] args) {
+        PackageData packageData = new PackageData();
+
+        // output String representation of packageData
+        System.out.printf("After instantiation:%n%s%n", packageData);
+
+        // change package access data in packageData object
+        packageData.number = 77;
+        packageData.string = "Goodbye";
+
+        // output String representation of packageData
+        System.out.printf("%nAfter changing values:%n%s%n", packageData);
+    }
+}
+
+
