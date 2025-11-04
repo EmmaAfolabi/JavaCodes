698db2b8370a60a29cc4d64b633567db3a6bd527
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-04 13:28:48 +0100

    PackageDataTest

--- src/Chapter8/PackageDataTest.java
+++ src/Chapter8/PackageDataTest.java
@@ -18,4 +18,13 @@ public class PackageDataTest {
     }
 }
 
+// class with package access instance variables
+class PackageData {
+    int number = 0; // package-access instance variable
+    String string = "Hello"; // package-access instance variable
 
+    // return PackageData object String representation
+    public String toString() {
+        return String.format("number: %d; string: %s", number, string);
+    }
+}
