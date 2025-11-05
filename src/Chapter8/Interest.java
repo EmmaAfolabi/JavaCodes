3b6707d4ce3d488349675d8719da9529b57ad9a2
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-05 10:26:55 +0100

    Interest

---
+++ src/Chapter8/Interest.java
@@ -0,0 +1,11 @@
+package Chapter8;
+// Compound-Interest calculations with BigDecimal
+import java.math.BigDecimal;
+
+public class Interest {
+    public static void main(String[] args) {
+        // initial principal amount before interest
+        BigDecimal principal = BigDecimal.valueOf(1000.0);
+        BigDecimal rate = BigDecimal.valueOf(0.05); // interest rate
+    }
+}
