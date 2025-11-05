6c24dfd261d06ea2ad9e2c79f784189f8f47c522
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-05 15:13:27 +0100

    Interest

--- src/Chapter8/Interest.java
+++ src/Chapter8/Interest.java
@@ -7,5 +7,15 @@ public class Interest {
         // initial principal amount before interest
         BigDecimal principal = BigDecimal.valueOf(1000.0);
         BigDecimal rate = BigDecimal.valueOf(0.05); // interest rate
+
+        // display headers
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+
+        // calculate amount on deposit for each of ten years
+        for (int year = 1; year <= 10; year++) {
+            // calculate new amount for specified year
+            BigDecimal amount =
+                    principal.multiply(rate.add(BigDecimal.ONE).pow(year));
+        }
     }
 }
