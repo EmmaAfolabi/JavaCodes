e58b7a76fd564e120444d96bf054967d201fcd55
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-13 14:49:12 +0100

    Cylinder

--- src/Chapter8/Exercises/Cylinder.java
+++ src/Chapter8/Exercises/Cylinder.java
@@ -22,8 +22,25 @@ public class Cylinder {
         if (radius > 0) {
             this.radius = radius;
         } else {
-            System.out.println("Error: Radius must be positive. Setting to default value 1.0");
+            System.out.println("Error!: Radius must be positive. Set value to 1.0");
             this.radius = 1.0;
         }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height > 0) {
+            this.height = height;
+        } else {
+            System.out.println("Error!: Height must be positive. Set value to 1.0");
+            this.height = 1.0;
+        }
+    }
+
+    public double getVolume() {
+        return Math.PI * radius * radius * height;
     }
 }
