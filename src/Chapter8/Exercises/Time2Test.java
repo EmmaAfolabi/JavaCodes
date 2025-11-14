1fcd89f55d571510eaec3ead4317ab24071f3913
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-14 13:01:44 +0100

    Time2Test

---
+++ src/Chapter8/Exercises/Time2Test.java
@@ -0,0 +1,13 @@
+package Chapter8.Exercises;
+
+public class Time2Test {
+
+    public static void main(String[] args) {
+
+        Time2 t1 = new Time2();
+        Time2 t2 = new Time2(2);
+        Time2 t3 = new Time2(21,34);
+        Time2 t4 = new Time2(11,59,59);
+        Time2 t5 = new Time2(t4);
+    }
+}
