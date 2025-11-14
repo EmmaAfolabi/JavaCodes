4eb528cba1ae6f7202eb5290ffe89fff989e2ce5
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-14 13:52:09 +0100

    SavingsAccountTest

---
+++ src/Chapter8/Exercises/SavingsAccountTest.java
@@ -0,0 +1,14 @@
+package Chapter8.Exercises;
+
+public class SavingsAccountTest {
+
+    public static void main(String[] args) {
+
+
+
+        SavingsAccount saver1 = new SavingsAccount(2000);
+        SavingsAccount saver2 = new SavingsAccount(3000);
+
+        SavingsAccount.modifyInterestRate(0.04);
+    }
+}
