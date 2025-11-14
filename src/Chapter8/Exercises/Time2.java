7c84192d2ba449a73e0ddad01345eeca22fbef19
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-14 11:52:39 +0100

    Time2

--- src/Chapter8/Exercises/Time2.java
+++ src/Chapter8/Exercises/Time2.java
@@ -32,5 +32,23 @@ public class Time2 {
         timeInSeconds = ((hour * 3600) + (minute * 60) + second);
     }
 
+    public Time2(Time2 time) {
+        this(time.getHour(), time.getMinute(), time.getSecond());
+    }
 
+    public void setTime(int hour, int minute, int second) {
+
+        if (hour < 0 || hour >= 24)
+            throw new IllegalArgumentException("hour must be 0 - 23");
+
+        if (minute < 0 || minute >= 60)
+            throw new IllegalArgumentException("minute must be 0 - 59");
+
+        if (second < 0 || second >= 60)
+            throw new IllegalArgumentException("second must be 0 - 59");
+
+        timeInSeconds = ((hour * 3600) + (minute * 60) + second);
+    }
+
+    public void
 }
