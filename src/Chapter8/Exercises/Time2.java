b41e10a6b9c4ef2aa5a2fb46195be5e1610cc257
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-14 12:57:13 +0100

    Time2

--- src/Chapter8/Exercises/Time2.java
+++ src/Chapter8/Exercises/Time2.java
@@ -50,5 +50,82 @@ public class Time2 {
         timeInSeconds = ((hour * 3600) + (minute * 60) + second);
     }
 
-    public void
+    public void setTime(int hour, int minute, int second) {
+
+        if (hour < 0 || hour >= 24)
+            throw new IllegalArgumentException("hour must be 0 - 23");
+
+        if (minute < 0 || minute >= 60)
+            throw new IllegalArgumentException("minute must be 0 - 59");
+
+        if (second < 0 || second >= 60)
+            throw new IllegalArgumentException("second must be 0 - 59");
+
+        timeInSeconds = ((hour * 3600) + (minute * 60) + second);
+    }
+
+    public void setHour(int hour) {
+
+        if (hour < 0 || hour >= 24)
+            throw new IllegalArgumentException("hour must be 0 - 23");
+
+        timeInSeconds = (hour * 3600);
+    }
+
+    public void setMinute(int minute) {
+
+        if (minute < 0 || minute >= 60) {
+            throw new IllegalArgumentException("minute must be 0 - 59");
+        }
+        timeInSeconds = minute * 60;
+    }
+
+    public void setSeconds(int second) {
+
+        if (second < 0 && second >= 60) {
+            throw new IllegalArgumentException("seconds must be 0 - 59");
+        }
+        timeInSeconds = second;
+    }
+
+    public int getHour() {
+        return timeInSeconds / 3600;
+    }
+
+    public int getMinute() {
+        int minute = (getHour() * 3600);
+        return (timeInSeconds - minute) / 60;
+    }
+
+    public int getSecond() {
+        int seconds = ((getHour() * 3600) + (getMinute() * 60));
+        return timeInSeconds - seconds;
+    }
+
+    public String toUniversalString() {
+        return String.format("%d:%02d:%02d: %s",
+                ((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12),
+                (getMinute() == 60 ? 00 : getMinute()), (getSecond() == 60 ? 00 : getSecond()),
+                (getHour() < 12 ? "AM" : "PM"));
+    }
+
+    public int tick() {
+        int seconds = getSecond() + 1;
+        return seconds;
+    }
+
+    public int incrementMinute() {
+        int minute = getMinute() + 1;
+        return minute;
+    }
+
+    public int incrementHour() {
+        int hour = getHour() + 1;
+        return hour;
+    }
+
+    public String newTime() {
+        return String.format("%02d:%02d:%02d%4s", (incrementHour() == 24) ? 00 : incrementHour(),
+                (tick()== 60)? 00 : getSecond(), (incrementHour() % 24 <= 11) ? "AM" : "PM");
+    }
 }
