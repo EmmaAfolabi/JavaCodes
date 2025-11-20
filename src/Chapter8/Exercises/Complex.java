816e30c3da831db5aa059a1b509a3dcb68f1b99b
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-20 18:51:21 +0100

    Complex

---
+++ src/Chapter8/Exercises/Complex.java
@@ -0,0 +1,15 @@
+package Chapter8.Exercises;
+
+public class Complex {
+
+    private double real;
+    private double imaginary;
+
+
+    public Complex(double real, double imaginary) {
+
+        this.real = real;
+        this.imaginary = imaginary;
+
+    }
+}
