6d88e1b664945e1806e215eda5597d5d27e6c6e9
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-20 19:13:10 +0100

    Complex

--- src/Chapter8/Exercises/Complex.java
+++ src/Chapter8/Exercises/Complex.java
@@ -12,4 +12,27 @@ public class Complex {
         this.imaginary = imaginary;
 
     }
+
+    public Complex() {
+
+        this(5, 7);
+
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
 }
