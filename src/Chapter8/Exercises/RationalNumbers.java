ee8162b8da8d810a180e54e259192e7ae4c2f4cb
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-21 00:30:41 +0100

    RationalNumbers

---
+++ src/Chapter8/Exercises/RationalNumbers.java
@@ -0,0 +1,12 @@
+package Chapter8.Exercises;
+
+public class RationalNumbers {
+
+    private int numerator;
+    private int denominator;
+
+    public RationalNumbers(int a, int b) {
+
+
+    }
+}
