3f76c5cb5f06b3aa9d78f2e916b18d3348f670da
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-21 11:58:40 +0100

    RationalNumbers

--- src/Chapter8/Exercises/RationalNumbers.java
+++ src/Chapter8/Exercises/RationalNumbers.java
@@ -52,5 +52,41 @@ public class RationalNumbers {
 
     }
 
-    public static RationalNumbers subFractions(RationalNumbers obj)
+    public static RationalNumbers subFractions(RationalNumbers object1, RationalNumbers object2) {
+
+        int deno = object1.getDenominator();
+        int deno1 = object2.getDenominator();
+
+        int num = object1.getNumerator();
+        int num1 = object2.getNumerator();
+
+        int lcm = findLCM(deno, deno1);
+
+        deno = lcm / deno;
+        deno1 = lcm / deno1;
+        num = deno * num;
+        num1 = deno1 * num1;
+        num = num - num1;
+
+        return new RationalNumbers(num, lcm);
+    }
+
+    public static RationalNumbers divide(RationalNumbers object1, RationalNumbers object2) {
+
+        int deno = object1.getDenominator();
+        int deno1 = object2.getDenominator();
+
+        int num = object1.getNumerator();
+        int num1 = object2.getNumerator();
+
+        int temp = num1;
+        num1 = deno1;
+        deno1 = temp;
+
+        int tempNum = num * num1;
+        int tempDeno = deno * deno1;
+
+        return new RationalNumbers(tempNum, tempDeno);
+
+    }
 }
