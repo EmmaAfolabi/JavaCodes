a498f763e9edfde23b3863f5637de233d4deda8e
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-21 18:24:10 +0100

    RationalNumbersTest

--- src/Chapter8/Exercises/RationalNumbersTest.java
+++ src/Chapter8/Exercises/RationalNumbersTest.java
@@ -9,6 +9,12 @@ public class RationalNumbersTest {
 
         RationalNumbers obj2 = new RationalNumbers(12, 6);
 
+        System.out.println(RationalNumbers.subFractions(obj1, obj2) + "\n");
 
+        System.out.println(RationalNumbers.multiply(obj1, obj2) + "\n");
+
+        System.out.println(RationalNumbers.addFractions(obj1, obj2) + "\n");
+
+        System.out.println(RationalNumbers.divide(obj1, obj2) + "\n");
     }
 }
