dca732db98570ad1191f198453c9050598534dc0
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-21 10:14:42 +0100

    RationalNumbers

--- src/Chapter8/Exercises/RationalNumbers.java
+++ src/Chapter8/Exercises/RationalNumbers.java
@@ -19,4 +19,38 @@ public class RationalNumbers {
 
 
     }
+
+    public RationalNumbers() {
+        this(8, 8);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public static RationalNumbers addFractions(RationalNumbers object1, RationalNumbers object2) {
+
+        int deno = object1.getDenominator();
+        int deno1 = object2.getDenominator();
+
+        int num = object1.getNumerator();
+        int num1 = object2.getNumerator();
+
+        int lcm = findLCM(deno, deno1);
+
+        deno /= lcm; deno1 /= lcm;
+
+        num *= deno; num1 *= deno1;
+
+        num = num + num1;
+
+        return new RationalNumbers(num, lcm);
+
+    }
+
+    public static
 }
