32ab498a96b0f10e5089b90a51757fe7230e9551
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-21 17:37:59 +0100

    RationalNumbersTest

---
+++ src/Chapter8/Exercises/RationalNumbersTest.java
@@ -0,0 +1,14 @@
+package Chapter8.Exercises;
+
+public class RationalNumbersTest {
+
+    public static void main(String[] args) {
+
+
+        RationalNumbers obj1 = new RationalNumbers(7, 5);
+
+        RationalNumbers obj2 = new RationalNumbers(12, 6);
+
+
+    }
+}
