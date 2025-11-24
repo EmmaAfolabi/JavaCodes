80bca89ab65af6d5b4ba596a192616682bcbce79
emmanuel Afolabi <daveemmanuel985@gmail.com>
2025-11-24 11:41:34 +0100

    Tic_Tac_Toe

--- src/Chapter8/Exercises/Tic_Tac_Toe.java
+++ src/Chapter8/Exercises/Tic_Tac_Toe.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class Tic_Tac_Toe {
+
     private static enum Type{X,O,EMPTY};
     private static enum players{human, computer}
 
@@ -12,4 +13,13 @@ public class Tic_Tac_Toe {
     Scanner input = new Scanner(System.in);
     SecureRandom random = new SecureRandom();
     private Type[][] board = new Type[3][3];
+
+    private int row_moves[] = {0,0,0,1,1,1,2,2,2};
+    private int col_moves[] = {0,1,2,0,1,2,0,1,2};
+
+    public Tic_Tac_Toe() {
+        for (int i = 0; i < board.length; i++)
+            for (int k = 0; k < board[i].length; k++)
+                board[i][k] = Type.EMPTY;
+    }
 }
